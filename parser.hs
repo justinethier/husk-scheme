033f34d10e87227a12a99c14473b440881294e47
justin <justin.ethier@gmail.com>
2010-05-26 17:35:43 -0400

    Added spaces processing

--- parser.hs
+++ parser.hs
@@ -1,3 +1,4 @@
+{- TODO: finish "lesson" 2, start again at Return Values -}
 module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
@@ -12,8 +13,11 @@ main = do
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
 	Left err -> "No match: " ++ show err
 	Right val -> "Found value"
 
