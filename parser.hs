54c109282e606540d0b31021ab13986e1129b2f7
justin <justin.ethier@gmail.com>
2010-05-27 13:21:07 -0400

    Built-out the "Return Values" section, but still need to do the exercises.

--- parser.hs
+++ parser.hs
@@ -1,5 +1,6 @@
 {- TODO: finish "lesson" 2, start again at Return Values -}
 module Main where
+import Control.Monad
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
@@ -9,6 +10,13 @@ main = do
 	args <- getArgs
 	putStrLn (readExpr (args !! 0))
 
+data LispVal = Atom String
+	| List [LispVal]
+	| DottedList [LispVal] LispVal
+	| Number Integer
+ 	| String String
+	| Bool Bool
+
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -16,8 +24,31 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseAtom :: Parser LispVal
+parseAtom = do
+	first <- letter <|> symbol
+	rest <- many (letter <|> digit <|> symbol)
+	let atom = first:rest
+	return $ case atom of
+		"#t" -> Bool True
+		"#f" -> Bool False
+		_    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseString :: Parser LispVal
+parseString = do
+	char '"'
+	x <- many (noneOf "\"")
+	char '"'
+	return $ String x
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
 	Left err -> "No match: " ++ show err
 	Right val -> "Found value"
 
