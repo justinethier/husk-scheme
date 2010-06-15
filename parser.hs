8a81f90143f9a3236af5ce6065f88d2d452ba868
justin <justin.ethier@gmail.com>
2010-06-14 22:40:10 -0400

    WIP

--- parser.hs
+++ parser.hs
@@ -234,13 +234,7 @@ eval (List (Atom "cond" : clauses)) =
 
 eval (List [Atom "case", key, clauses]) = 
     do let ekey = eval key
-       --test <- case c of
-       --  List (Atom "else" : exprs) -> eval $ Bool True
-       --  List (List(cond) : exprs) -> any (ekey==) cond -- TODO: search for key in cond
-       --case test of
-       --  Bool True -> evalCond c -- might just work...
-       --  otherwise -> eval $ List [Atom "case", key, cs] -- TODO: this is an *extremely* inefficient implementation!!
-	   evalCase(key, clauses)
+       evalCase(ekey, clauses)
 
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
@@ -251,8 +245,15 @@ evalCond (List [test, expr]) = eval expr
 evalCond (List (test : expr)) = last $ map eval expr -- TODO: all expr's need to be evaluated, not sure happening right now
 evalCond badForm = throwError $ BadSpecialForm "evalCond: Unrecognized special form" badForm
 
-evalCase key List(c:cs) = 
-    do let found = 
+--evalCase :: LispVal -> LispVal -> ThrowsError LispVal
+evalCase key (List (c:cs)) = 
+    do test <- case c of
+         List (Atom "else" : exprs) -> eval $ Bool True
+         List (List(cond) : exprs) -> eval $ Bool $ any (key==) cond -- TODO: search for key in cond
+       case test of
+         Bool True -> evalCond c -- might just work...
+         otherwise -> eval $ List [Atom "case", key, cs]
+evalCase key badForm = throwError $ BadSpecialForm "evalCase: Unrecognized special form" badForm
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
