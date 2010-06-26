ba53713e5a2e7b5ae93c152b690fa9046911a7c8
justin <justin.ethier@gmail.com>
2010-06-26 12:05:21 -0400

    Added test cases

--- test-scripts/t-cond.sh
+++ test-scripts/t-cond.sh
@@ -1,1 +1,8 @@
-TODO
+# greater
+./skim "(cond ((> 3 2) 'greater) ((< 3 2) 'less))" 
+# equal
+./skim "(cond ((> 3 3) 'greater) ((< 3 3) 'less) (else 'equal))"
+
+# TODO: => special form
+#./skim "(cond ((assv 'b '((a 1) (b 2))) => cadr)
+#                        (else #f))                 ===>  2
