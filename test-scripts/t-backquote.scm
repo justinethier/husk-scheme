fc5a96e1b41ee2983004faf64ac7799215d8e88e
justin <justin.ethier@gmail.com>
2010-07-02 16:41:26 -0400

    Initial version

---
+++ test-scripts/t-backquote.scm
@@ -0,0 +1,24 @@
+(load "skim-unit.scm")
+
+(assert-equal (lambda () ( `(list ,(+ 1 2) 4)))
+			  (list 3 4))
+
+(assert-equal (lambda () ( (let ((name 'a)) `(list ,name ',name))))
+              (list a (quote a)))
+
+(assert-equal (lambda () (
+			   `(a ,(+ 1 2) ,@(map abs '(4 -5 6)) b)))
+				(a 3 4 5 6 b))
+
+(assert-equal (lambda () (
+				`(( foo ,(- 10 3)) ,@(cdr '(c)) . ,(car '(cons)))))
+                 ((foo 7) . cons))
+
+; TODO: needs vector support
+;(assert-equal (lambda () (
+;				`#(10 5 ,(sqrt 4) ,@(map sqrt '(16 9)) 8)))
+;				#(10 5 2 4 3 8))
+
+; TODO: more test cases from:
+; http://www.schemers.org/Documents/Standards/R5RS/HTML/r5rs-Z-H-7.html#%_sec_4.2.6
+
