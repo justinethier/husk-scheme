c4fe66d2b6eafa0ca3d006776b22826703003138
justin <justin.ethier@gmail.com>
2010-07-03 14:29:21 -0400

    Added all functions from the Chapter.

--- stdlib.scm
+++ stdlib.scm
@@ -51,9 +51,18 @@
 (define fold foldl)
 (define reduce fold)
 
+(define (unfold func init pred)
+  (if (pred init)
+      (cons init '())
+      (cons init (unfold func (func init) pred))))
 
-; TODO: start here with unfold
+(define (sum . lst)     (fold + 0 lst))
+(define (product . lst) (fold * 1 lst))
+(define (and . lst)     (fold && #t lst))
+(define (or . lst)      (fold || #f lst))
 
+(define (max first . rest) (fold (lambda (old new) (if (> old new) old new)) first rest))
+(define (min first . rest) (fold (lambda (old new) (if (< old new) old new)) first rest))
 
 (define zero?        (curry = 0))
 (define positive?    (curry < 0))
@@ -61,4 +70,17 @@
 (define (odd? num)   (= (mod num 2) 1))
 (define (even? num)  (= (mod num 2) 0))
 
+(define (length lst)    (fold (lambda (x y) (+ x 1)) 0 lst))
+(define (reverse lst)   (fold (flip cons) '() lst))
+
+(define (mem-helper pred op)  (lambda (acc next) (if (and (not acc) (pred (op next))) next acc)))
+(define (memq obj lst)        (fold (mem-helper (curry eq? obj) id) #f lst))
+(define (memv obj lst)        (fold (mem-helper (curry eqv? obj) id) #f lst))
+(define (member obj lst)      (fold (mem-helper (curry equal? obj) id) #f lst))
+(define (assq obj alist)      (fold (mem-helper (curry eq? obj) car) #f alist))
+(define (assv obj alist)      (fold (mem-helper (curry eqv? obj) car) #f alist))
+(define (assoc obj alist)     (fold (mem-helper (curry equal? obj) car) #f alist))
+
+(define (map func lst)        (foldr (lambda (x y) (cons (func x) y)) '() lst))
+(define (filter pred lst)     (foldr (lambda (x y) (if (pred x) (cons x y) y)) '() lst))
 
