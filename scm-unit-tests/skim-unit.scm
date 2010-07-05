232f09206f9a88a74225daea7e9613b389a36e77
justin <justin.ethier@gmail.com>
2010-07-04 21:27:04 -0400

    Changed the wording

--- scm-unit-tests/skim-unit.scm
+++ scm-unit-tests/skim-unit.scm
@@ -1,8 +1,8 @@
 (load "../stdlib.scm")
 
-(define (unit-test-handler expected result) 
-  (if (not (eqv? expected result))
-    (write (list "Test failed. Expected: " expected " Observed: " result))
+(define (unit-test-handler expected actual) 
+  (if (not (eqv? expected actual))
+    (write (list "Test failed; expected value: " expected ", actual value: " actual))
     #t))
 
 (define (assert proc) (unit-test-handler #t (proc)))
