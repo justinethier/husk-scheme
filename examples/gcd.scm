d3ff5220d5086e0e1056997233bd39e52e2fd452
justin <justin.ethier@gmail.com>
2010-07-06 00:02:08 -0400

    Read args from command line, general cleanup.

--- examples/gcd.scm
+++ examples/gcd.scm
@@ -2,10 +2,14 @@
 ; Greatest common denominator
 ; Based on example code from SCIP
 ;
+(load "stdlib.scm")
+
 (define (gcd a b)
   (if (= b 0)
       a
       (gcd b (remainder a b))))
 
-; TODO: load argv into (main arguments)
-(gcd 2010 42)
+(define (main args)
+  (write (gcd
+    (string->number (list-ref args 1))
+    (string->number (list-ref args 2)))))
