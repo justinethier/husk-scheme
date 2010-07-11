3047d690d8c9a39a2756feab985bdbee32deea48
justin <justin.ethier@gmail.com>
2010-07-11 17:13:57 -0400

    Initial version

---
+++ examples/macros.scm
@@ -0,0 +1,9 @@
+;(let ((x 1)) ;; - with this form, csi transforms (define-syntax) into (letrec-syntax), since
+;             ;;   the define form is only allowed at the top-level.
+(define-syntax test (syntax-rules () ((print ...) (print 1))
+                                     ((print ... ...) (print 2))  
+                      ))
+;)
+(test 1)
+(test 2)
+(test 1 2)
