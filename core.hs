a99279ecb9586d2d55757ce05d86dac9fbfff70f
justin <justin.ethier@gmail.com>
2010-07-13 13:25:28 -0400

    Added more higher-level TODO items.

--- core.hs
+++ core.hs
@@ -7,6 +7,16 @@
  -
  - -}
 
+{-
+ - TODO: 
+ -
+ - => compare my functions against those listed on 
+ -    http://en.wikipedia.org/wiki/Scheme_(programming_language)
+ -
+ - => think about how to implement hashtable in scheme 
+ -    (http://www.math.grin.edu/~stone/events/scheme-workshop/hash-tables.html)
+ -
+ - -}
 
 {-
  - TODO: add macro support.
