2872b506c4bcf8ef442b3c4ce960bb0042503c8d
justin <justin.ethier@gmail.com>
2010-07-26 13:45:37 -0400

    Stubbed out more of the macro code, but it's a mess right now...

--- core.hs
+++ core.hs
@@ -456,7 +456,29 @@ findMatch _ _ rules _ = throwError $ BadSpecialForm "Malformed syntax-rules" (St
 -- TODO: localEnv is an env just used for this invocation (kind of like the Env in lambda)
 -- TODO: we are ignoring ... for the moment
 --matchRule :: Env -> Env -> LispVal -> LispVal -> LispVal
-matchRule env localEnv patternVar inputVar = return $ String "TODO"
+matchRule env localEnv patternVar inputVar@(List (Atom _ : is)) =
+  if (length patternVar) == (length inputVar) -- temporary clause 
+    then case patternVar of 
+      List (Atom _ : ps) -> do
+        match <- loadLocal localEnv ps is [] --mapM (loadLocal localEnv)
+        return $ String "TODO"
+      otherwise -> throwError $ BadSpecialForm "Malformed rule in syntax-rules" patternVar
+    else return $ Nil ""
+  where loadLocal localEnv pattern input result = 
+          if (length input) == 1
+            then -- check input against pattern (both should be single var)
+            else -- chect first input against first pattern, recurse...
+        checkLocal localEnv (Bool pattern) (Bool input) = True
+        checkLocal localEnv (Number pattern) (Number input) = True
+        checkLocal localEnv (Float pattern) (Float input) = True
+        checkLocal localEnv (String pattern) (String input) = True
+        checkLocal localEnv (Char pattern) (Char input) = True
+
+-- TODO, load into localEnv in some (all?) cases?: eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
+-- TODO: eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+-- TODO: eqv [(Vector arg1), (Vector arg2)] = eqv [List $ (elems arg1), List $ (elems arg2)] 
+-- TODO: eqv [l1@(List arg1), l2@(List arg2)] = eqvList eqv [l1, l2]
+        checkLocal localEnv _ _ = False
 
 -- TODO - high-level approach:
 -- input is List(Atom : xs)
