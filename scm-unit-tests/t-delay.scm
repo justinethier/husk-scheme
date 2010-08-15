cdbba46c67b8f5256f35feb76f8067940fe43eaf
justin <justin.ethier@gmail.com>
2010-08-15 15:00:54 -0400

    Fleshed this out with some tests.

--- scm-unit-tests/t-delay.scm
+++ scm-unit-tests/t-delay.scm
@@ -1,1 +1,14 @@
 ; TODO: test cases for delayed evaluation
+(load "skim-unit.scm")
+
+(assert-equal (lambda () (force (delay 1)))
+			  1)
+(define count 0)
+(define x 0)
+(define p (delay (begin (set! count (+ count 1))
+                        (if (> count x)
+                            count
+                            (force p)))))
+
+(unit-test-handler-results)
+
