b29a7a602d9bf528f1de5d91572a9ddc29598078
justin <justin.ethier@gmail.com>
2010-08-17 12:54:34 -0400

    Renamed (let) to (my-let) so that it does not overwrite (let) from stdlib.

--- scm-unit-tests/t-macro.scm
+++ scm-unit-tests/t-macro.scm
@@ -1,27 +1,15 @@
 ; TODO: bug - if the first line of the file is blank, (load) spits out a Prelude.last error!
 (load "skim-unit.scm")
 
-;
-;(define-syntax let
-;  (syntax-rules ()
-;    ((_ ((x v) ...) e1 e2 ...)
-;    ((lambda (x ...) e1 e2 ...) v ...))))
-;
-;(let ((x 9)) (print x) (print (+ x 9)))
-; TODO: "let" tests
-;
-;(define-syntax let (syntax-rules ()    ((_ x)    (x))))
-
-
-(define-syntax let 
+(define-syntax my-let 
   (syntax-rules ()
-    ((let x)
+    ((my-let x)
 	(begin x))))
 (define x "hello, world")
 
-(assert-equal (lambda () (let 2)) 2)
-(assert-equal (lambda () (let x)) "hello, world")
-(assert-equal (lambda () (let (+ 1 2 3 4 5))) 15)
+(assert-equal (lambda () (my-let 2)) 2)
+(assert-equal (lambda () (my-let x)) "hello, world")
+(assert-equal (lambda () (my-let (+ 1 2 3 4 5))) 15)
 
 
 (define-syntax test (syntax-rules () ((test 1 ...) (list 1))))
@@ -45,7 +33,7 @@
 (assert-equal (lambda () (test 1 2 3)) '(1 2 3))
 (assert-equal (lambda () (test 1 1 1 2 2 2 3)) '(1 2 3))
 
-; TODO - get each of these working next, then maybe try the real (let):
+; TODO - get each of these working next, then maybe try the real (my-let):
 (define-syntax test (syntax-rules () ((test x ...) (list 1))))
 (assert-equal (lambda () (test "hello, world!" (+ 1 2 3) x)) '(1))
 (assert-equal (lambda () (test "hello, world!" 1 2 3)) '(1))
@@ -63,19 +51,14 @@
 (define-syntax test (syntax-rules () ((_ (1 2) (3 . x)) (list x))))
 (assert-equal (lambda () (test (1 2) (3 . 4))) '(4))
 
-; "Fake" let test
-(define-syntax let
+; "Fake" my-let test
+(define-syntax my-let
   (syntax-rules ()
     ((_ e1 ...)
     ((lambda () e1 ...)))))
-(assert-equal (lambda () (let (+ 1 2))) 3)
+(assert-equal (lambda () (my-let (+ 1 2))) 3)
 
 ; The 'real' let:
-(define-syntax let
-  (syntax-rules ()
-    ((_ ((x v) ...) e1 e2 ...)
-    ((lambda (x ...) e1 e2 ...) v ...))))
-
 (assert-equal (lambda () (let ((x 1) (y 2) (z 3)) (+ x y z))) 6)
 (assert-equal (lambda () (let ((x 11) (y 22) (z 34)) (+ x y z))) 67)
 (assert-equal (lambda () (let ((x (* 1 2 3 4)) (y 22) (z 34)) (+ x y z))) (+ 24 22 34))
@@ -84,18 +67,19 @@
 (assert-equal (lambda () ((lambda () (let ((x 1)) x)))) 1)
 
 ; TODO: named let
-(let loop ((numbers '(3 -2 1 6 -5))
-           (nonneg '())
-           (neg '()))
-    (cond ((null? numbers) (list nonneg neg))
-          ((>= (car numbers) 0)
-           (loop (cdr numbers)
-                 (cons (car numbers) nonneg)
-                  neg))
-          ((< (car numbers) 0)
-           (loop (cdr numbers)
-                 nonneg
-                 (cons (car numbers) neg)))))
+;(let loop ((numbers '(3 -2 1 6 -5))
+;(let loop ((numbers '(3 2 1 6 5))
+;           (nonneg '())
+;           (neg '()))
+;    (cond ((null? numbers) (list nonneg neg))
+;          ((>= (car numbers) 0)
+;           (loop (cdr numbers)
+;                 (cons (car numbers) nonneg)
+;                  neg))
+;          ((< (car numbers) 0)
+;           (loop (cdr numbers)
+;                 nonneg
+;                 (cons (car numbers) neg)))))
 
 
 ; TODO:
