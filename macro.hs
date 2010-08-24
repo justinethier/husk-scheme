5929442881b96220757e51a70239868fe03e2bbd
justin <justin.ethier@gmail.com>
2010-08-24 13:19:57 -0400

    Added as placeholder for relocating code from core.

---
+++ macro.hs
@@ -0,0 +1,12 @@
+{-
+ - skim-scheme
+ - Macro
+ -
+ - This file contains 
+ -
+ - @author Justin Ethier
+ -
+ - -}
+module Skim.Macro where -- Bad name due to  conflicts??
+
+-- TODO: relocate hygenic macro code from core
---
+++ parser.hs
@@ -0,0 +1,12 @@
+{-
+ - skim-scheme
+ - Parser
+ -
+ - This file contains 
+ -
+ - @author Justin Ethier
+ -
+ - -}
+module Skim.Parser where -- Bad name due to  conflicts??
+
+-- TODO: relocate parser code from core
