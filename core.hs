705157ef2b98b50e640ea975a585bb9056a7d4f0
justin <justin.ethier@gmail.com>
2010-08-24 13:07:04 -0400

    Relocated data types to types.hs

--- core.hs
+++ core.hs
@@ -20,6 +20,7 @@
  - -}
 
 module Main where
+import Skim.Types
 import Control.Monad
 import Control.Monad.Error
 import Char
@@ -83,22 +84,6 @@ readExpr = readOrThrow parseExpr
 readExprList = readOrThrow (endBy parseExpr spaces)
 
 {- Variables Section -}
-type Env = IORef [((String, String), IORef LispVal)] -- lookup via: (namespace, variable)
-type IOThrowsError = ErrorT LispError IO
-
-nullEnv :: IO Env
-nullEnv = newIORef []
-
-liftThrows :: ThrowsError a -> IOThrowsError a
-liftThrows (Left err) = throwError err
-liftThrows (Right val) = return val
-
-runIOThrows :: IOThrowsError String -> IO String
-runIOThrows action = runErrorT (trapError action) >>= return . extractValue
-
-
-macroNamespace = "m"
-varNamespace = "v"
 
 -- Determine if a variable is bound in the "variable" namespace
 isBound :: Env -> String -> IO Bool
@@ -150,80 +135,6 @@ bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
   where extendEnv bindings env = liftM  (++ env) (mapM addBinding bindings)
         addBinding (var, value) = do ref <- newIORef value
                                      return (var, ref)
-
-{- Error handling code -}
-data LispError = NumArgs Integer [LispVal]
-  | TypeMismatch String LispVal
-  | Parser ParseError
-  | BadSpecialForm String LispVal
-  | NotFunction String String
-  | UnboundVar String String
-  | Default String
-
-showError :: LispError -> String
-showError (NumArgs expected found) = "Expected " ++ show expected
-                                  ++ " args; found values " ++ unwordsList found
-showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
-                                  ++ ", found " ++ show found
-showError (Parser parseErr) = "Parse error at " ++ ": " ++ show parseErr
-showError (BadSpecialForm message form) = message ++ ": " ++ show form
-showError (NotFunction message func) = message ++ ": " ++ show func
-showError (UnboundVar message varname) = message ++ ": " ++ varname
-
-instance Show LispError where show = showError
-instance Error LispError where
-  noMsg = Default "An error has occurred"
-  strMsg = Default
-
-type ThrowsError = Either LispError
-
-trapError action = catchError action (return . show)
-
-extractValue :: ThrowsError a -> a
-extractValue (Right val) = val
-
-data LispVal = Atom String
-	| List [LispVal]
-	| DottedList [LispVal] LispVal
-	| Vector (Array Int LispVal)
-	| Number Integer
-	| Float Float
- 	| String String
-	| Char Char
-	| Bool Bool
-	| PrimitiveFunc ([LispVal] -> ThrowsError LispVal)
-	| Func {params :: [String], vararg :: (Maybe String),
-	        body :: [LispVal], closure :: Env}
-	| IOFunc ([LispVal] -> IOThrowsError LispVal)
-	| Port Handle
-    | Nil String -- String is probably wrong type here, but OK for now (do not expect to use this much, just internally)
-
-showVal :: LispVal -> String
-showVal (Nil _) = ""
-showVal (String contents) = "\"" ++ contents ++ "\""
-showVal (Char chr) = [chr]
-showVal (Atom name) = name
-showVal (Number contents) = show contents
-showVal (Float contents) = show contents
-showVal (Bool True) = "#t"
-showVal (Bool False) = "#f"
-showVal (Vector contents) = "#(" ++ (unwordsList $ Data.Array.elems contents) ++ ")"
-showVal (List contents) = "(" ++ unwordsList contents ++ ")"
-showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
-showVal (PrimitiveFunc _) = "<primitive>"
-showVal (Func {params = args, vararg = varargs, body = body, closure = env}) = 
-  "(lambda (" ++ unwords (map show args) ++
-    (case varargs of
-      Nothing -> ""
-      Just arg -> " . " ++ arg) ++ ") ...)"
-showVal (Port _) = "<IO port>"
-showVal (IOFunc _) = "<IO primitive>"
-
-unwordsList :: [LispVal] -> String
-unwordsList = unwords . map showVal
-
-{- Allow conversion of lispval instances to strings -}
-instance Show LispVal where show = showVal
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>?@^_~" -- TODO: I removed #, make sure this is OK w/spec, and test cases
