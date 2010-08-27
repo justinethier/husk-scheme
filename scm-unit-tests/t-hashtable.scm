a8b22380d1be8c3f1de5617563863371ff5f787a
justin <justin.ethier@gmail.com>
2010-08-27 12:40:59 -0400

    Temporary fix for hashing problems...

--- scm-unit-tests/t-hashtable.scm
+++ scm-unit-tests/t-hashtable.scm
@@ -2,6 +2,7 @@
 
 (define ht (make-hash-table))
 (hash-table-set! ht 1 1)
+(assert-equal (lambda () (hash-table-ref ht 1)) 1)
 (hash-table-set! ht 2 2)
 (hash-table-set! ht 3 3)
 (hash-table-set! ht 4 4)
@@ -9,7 +10,6 @@
 (hash-table-set! ht 1 "test")
 (hash-table-set! ht "test2" "testing")
 
-(assert-equal (lambda () (hash-table-ref ht 1)) 1)
 (assert-equal (lambda () (hash-table-ref ht 1)) "test")
 (assert-equal (lambda () (hash-table-ref ht "test2")) "testing")
 
@@ -18,7 +18,7 @@
 (assert-equal (lambda () (hash-table-exists? ht 1)) #t)
 (assert-equal (lambda () (hash-table-exists? ht "test2")) #t)
 (assert-equal (lambda () (hash-table-exists? ht "test3")) #f)
-(assert-equal (lambda () (hash-table-size ht)) 3)
+(assert-equal (lambda () (hash-table-size ht)) 6)
 
 ; TODO: many more functions now, cross-reference core code and add more cases...
 (unit-test-handler-results)
