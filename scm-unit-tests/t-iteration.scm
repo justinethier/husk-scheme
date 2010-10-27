ae80c269aaf144dbdf43679ae47b14b56eac0cda
justin <justin.ethier@gmail.com>
2010-10-27 16:48:02 -0400

    Added formal test cases. do macro almost works, just need to handle special case where a 'step' is not specified.

--- scm-unit-tests/t-iteration.scm
+++ scm-unit-tests/t-iteration.scm
@@ -5,25 +5,27 @@
 (define-syntax do
   (syntax-rules ()
      ((_ ((var init step) ...)
-          (test expr ...) 
+         (test expr ...) 
           command ...)
      (let loop ((var init) ...)
        (if test
          (begin expr ...)
-         ((begin command ...)
+         (begin (begin command ...)
           (loop step ...)))))))
 
-;(do ((vec (make-vector 5) vec)
-;          (i 0 (+ i 1)))
-;      ((= i 5) vec)
-;        (vector-set! vec i i))
-;; ===>  #(0 1 2 3 4)
+; TODO: should not need 'vec' at the end of first line; see R5RS spec
+(assert-equal (lambda () 
+                (do ((vec (make-vector 5) vec)
+                     (i 0 (+ i 1)))
+                    ((= i 5) vec)
+                     (vector-set! vec i i)))
+                #(0 1 2 3 4))
 
-(let ((x '(1 3 5 7 9)))
-    (do ((x x (cdr x))
-         (sum 0 (+ sum (car x))))
-        ((null? x) sum)))
-;; ===>  25
+(assert-equal (lambda () 
+                (let ((x '(1 3 5 7 9)))
+                    (do ((x x (cdr x))
+                         (sum 0 (+ sum (car x))))
+                        ((null? x) sum))))
+                25)
 
-;(assert-equal (lambda () (hash-table-ref ht 1)) 1)
-;(unit-test-handler-results)
+(unit-test-handler-results)
