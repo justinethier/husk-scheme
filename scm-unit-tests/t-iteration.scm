e97cc06f3868f1ce3e829f58b268558717da8eff
justin <justin.ethier@gmail.com>
2010-10-26 22:44:46 -0400

    Added initial macro for (do)

--- scm-unit-tests/t-iteration.scm
+++ scm-unit-tests/t-iteration.scm
@@ -1,15 +1,27 @@
 (load "skim-unit.scm")
-;
-;(do ((vec (make-vector 5))
-;          (i 0 (+ i 1)))
-;      ((= i 5) vec)
-;        (vector-set! vec i i))                  ===>  #(0 1 2 3 4)
-;
-(let ((x '(1 3 5 7 9)))
-    (do ((x x (cdr x))
-                (sum 0 (+ sum (car x))))
-            ((null? x) sum)))                     ===>  25
 
+(define-syntax do
+  (syntax-rules ()
+    ((_ ((var init step) ...)
+        (test exp ...)
+        command ...)
+     ((begin (set! var init) ...)
+      (if test 
+        ((begin command ...)
+         "TODO: iterate, call into (do)")
+        (begin command ...))))))
+
+(do ((vec (make-vector 5))
+          (i 0 (+ i 1)))
+      ((= i 5) vec)
+        (vector-set! vec i i))                  ===>  #(0 1 2 3 4)
+
+;(let ((x '(1 3 5 7 9)))
+;    (do ((x x (cdr x))
+;                (sum 0 (+ sum (car x))))
+;            ((null? x) sum)))                     ===>  25
+;
+;
 ;(assert-equal (lambda () (hash-table-ref ht 1)) 1)
 
 (unit-test-handler-results)
