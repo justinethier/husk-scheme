770b85d33a0229766e9b32609a197d73818f8e94
justin <justin.ethier@gmail.com>
2010-10-27 13:33:17 -0400

    Working through a macro for do, will migrate into stdlib once it's working.

--- scm-unit-tests/t-iteration.scm
+++ scm-unit-tests/t-iteration.scm
@@ -1,27 +1,29 @@
 (load "skim-unit.scm")
 
+; TODO: will need to make step optional on a per-variable basis
+; TODO: need to firm up macro code a bit, mostly on error reporting / crashing side...
 (define-syntax do
   (syntax-rules ()
-    ((_ ((var init step) ...)
-        (test exp ...)
-        command ...)
-     ((begin (set! var init) ...)
-      (if test 
-        ((begin command ...)
-         "TODO: iterate, call into (do)")
-        (begin command ...))))))
+     ((_ ((var init step) ...)
+          (test expr ...) 
+          command ...)
+     (let loop ((var init) ...)
+       (if test
+         (begin expr ...)
+         ((begin command ...)
+          (loop step ...)))))))
 
-(do ((vec (make-vector 5))
-          (i 0 (+ i 1)))
-      ((= i 5) vec)
-        (vector-set! vec i i))                  ===>  #(0 1 2 3 4)
+;(do ((vec (make-vector 5) vec)
+;          (i 0 (+ i 1)))
+;      ((= i 5) vec)
+;        (vector-set! vec i i))
+;; ===>  #(0 1 2 3 4)
 
-;(let ((x '(1 3 5 7 9)))
-;    (do ((x x (cdr x))
-;                (sum 0 (+ sum (car x))))
-;            ((null? x) sum)))                     ===>  25
-;
-;
+(let ((x '(1 3 5 7 9)))
+    (do ((x x (cdr x))
+         (sum 0 (+ sum (car x))))
+        ((null? x) sum)))
+;; ===>  25
+
 ;(assert-equal (lambda () (hash-table-ref ht 1)) 1)
-
-(unit-test-handler-results)
+;(unit-test-handler-results)
