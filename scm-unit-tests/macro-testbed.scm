fbbf97edf3fd0212c74b5465d884004280a443ea
justin <justin.ethier@gmail.com>
2010-11-12 02:11:54 -0500

    Initial version

---
+++ scm-unit-tests/macro-testbed.scm
@@ -0,0 +1,26 @@
+(load "stdlib.scm")
+
+; Problem below is (list ...)
+; our code is not smart enough to know that list is not
+; a variable that should be transformed, so (guessing) that
+; it returns Nil for list and that stops the macro transform...
+(define-syntax my-do
+  (syntax-rules ()
+     ((_ ((var init . step) ...)
+         (test expr ...) 
+          command ...)
+     (let loop ((var init) ...)
+       (if test
+         (begin expr ...)
+         (begin (begin command ...)
+                (quote (loop 
+                      (list var . step) ...))))))))
+;                (loop 
+;                  (if (null? (cdr (list var . step))) 
+;                      (car  (list var . step))
+;                      (cadr (list var . step))) ...)))))))
+(write
+                (my-do ((vec (make-vector 5) vec)
+                     (i 0 (+ i 1)))
+                    ((= i 5) vec)
+                     (vector-set! vec i i)))
