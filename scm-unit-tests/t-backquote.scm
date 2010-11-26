f8c75d41beb002771eccd46963b9a9535d755249
justin <justin.ethier@gmail.com>
2010-11-26 18:59:43 -0500

    Added another piece of test code.

--- scm-unit-tests/t-backquote.scm
+++ scm-unit-tests/t-backquote.scm
@@ -17,6 +17,10 @@
 ;				`#(10 5 ,(sqrt 4) ,@(map sqrt '(16 9)) 8)))
 ;				#(10 5 2 4 3 8))
 
+; TODO:
+; `(1 2 . ,(list 3 4))
+; csi returns - (1 2 3 4)
+
 ; TODO: more test cases from:
 ; http://www.schemers.org/Documents/Standards/R5RS/HTML/r5rs-Z-H-7.html#%_sec_4.2.6
 (unit-test-handler-results)
