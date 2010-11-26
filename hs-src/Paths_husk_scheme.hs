7a9f7372c237cf1988450ef4bb0ccbcd80ca5901
justin <justin.ethier@gmail.com>
2010-11-26 17:54:37 -0500

    Initial version

---
+++ hs-src/Paths_husk_scheme.hs
@@ -0,0 +1,5 @@
+-- |Placeholder for auto-generated module created by cabal. See: <http://neilmitchell.blogspot.com/2008/02/adding-data-files-using-cabal.html>
+module Paths_husk_scheme where
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName = return
