1ceec7f9cfbc4974699683539f40488f73112bde
justin <justin.ethier@gmail.com>
2010-12-18 11:38:44 -0500

    Added test cases for call/cc

--- scm-unit-tests/t-cont.scm
+++ scm-unit-tests/t-cont.scm
@@ -18,5 +18,10 @@
 (assert/equal (f (lambda (x) x)) 3) 
 (assert/equal (call/cc f) 2)
 
+(define (f return)
+    (return (+ 1 2 3 (+ 4 5 6)))
+      3)
+
+(assert/equal (call/cc f) (+ 1 2 3 4 5 6))
 
 (unit-test-handler-results)
