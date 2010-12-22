1f76b11443edeea370775f9dd6079a367bc70793
justin <justin.ethier@gmail.com>
2010-12-22 00:53:34 -0500

    Added tests

--- scm-unit-tests/t-cont.scm
+++ scm-unit-tests/t-cont.scm
@@ -27,4 +27,31 @@
 
 (assert/equal (call/cc procedure?) #t)
 (assert/equal (call-with-current-continuation procedure?) #t)
+
+; TODO:
+;(call-with-current-continuation
+;    (lambda (exit)
+;          (for-each (lambda (x)
+;                            (if (negative? x)
+;                                (exit x)))
+;                   '(54 0 37 -3 245 19))
+;              #t))
+;===>  -3
+
+(define list-length
+  (lambda (obj)
+    (call-with-current-continuation
+      (lambda (return)
+              (letrec ((r
+                (lambda (obj)
+                  (cond ((null? obj) 0)
+                        ((pair? obj) 
+                         (+ (r (cdr obj)) 1))
+                        (else (return #f))))))
+        (r obj))))))
+
+(list-length '(1 2 3 4))                    ===>  4
+
+(list-length '(a b . c))                    ===>  #f
+
 (unit-test-handler-results)
