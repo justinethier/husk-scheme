7e9350295a9b82fe3be0f08613ab4ca4a681c581
justin <justin.ethier@gmail.com>
2010-12-30 16:54:24 -0500

    Fixed warnings in definition of string2Symbol.

--- hs-src/Scheme/Core.hs
+++ hs-src/Scheme/Core.hs
@@ -981,8 +981,10 @@ symbol2String ([Atom a]) = return $ String a
 symbol2String [notAtom] = throwError $ TypeMismatch "symbol" notAtom
 
 string2Symbol :: [LispVal] -> ThrowsError LispVal
+string2Symbol [] = throwError $ NumArgs 1 []
 string2Symbol ([String s]) = return $ Atom s
 string2Symbol [notString] = throwError $ TypeMismatch "string" notString
+string2Symbol args@(_ : _) = throwError $ NumArgs 1 args
 
 isChar :: [LispVal] -> ThrowsError LispVal
 isChar ([Char _]) = return $ Bool True
