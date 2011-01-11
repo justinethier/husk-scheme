5d03240a38ba1c04d7621df5e2554b0f181cb241
justin <justin.ethier@gmail.com>
2011-01-11 04:23:47 -0500

    Initial version

---
+++ examples/yin-yang.scm
@@ -0,0 +1,6 @@
+(load "stdlib.scm")
+(let* ((yin
+        ((lambda (cc) (display "@") cc) (call-with-current-continuation (lambda (c) c))))
+       (yang
+        ((lambda (cc) (display "*") cc) (call-with-current-continuation (lambda (c) c)))))
+      (yin yang))
