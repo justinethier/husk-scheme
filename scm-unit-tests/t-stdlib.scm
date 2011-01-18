bda5dc2b598afa1779129208108172af6a03d124
justin <justin.ethier@gmail.com>
2011-01-18 17:42:00 -0500

    Added many new test cases from the R5RS spec.

--- scm-unit-tests/t-stdlib.scm
+++ scm-unit-tests/t-stdlib.scm
@@ -12,6 +12,50 @@
 ;
 ;
 
+; Primitive expression types, tests from R5RS spec
+; (consider moving to a separate file)
+(define x 28)
+(assert/equal x 28)
+
+(assert/equal 'a  'a)
+(assert/equal '#(a b c) '#(a b c))
+(assert/equal '() '())
+(assert/equal '(+ 1 2) '(+ 1 2))
+(assert/equal '(quote a) '(quote a))
+(assert/equal ''a       '(quote a))
+
+(assert/equal '"abc"  "abc")
+(assert/equal '145932 145932)
+(assert/equal '#t  #t)
+
+(assert/equal (+ 3 4)  
+              7)
+(assert/equal ((if #f + *) 3 4)  
+              12)
+
+(assert/equal ((lambda (x) (+ x x)) 4)  8)
+
+(define reverse-subtract
+    (lambda (x y) (- y x)))
+(assert/equal (reverse-subtract 7 10)  3)
+
+(define add4
+    (let ((x 4))
+          (lambda (y) (+ x y))))
+(assert/equal (add4 6)  10)
+
+(assert/equal (if (> 3 2) 'yes 'no)   'yes)
+(assert/equal (if (> 2 3) 'yes 'no)   'no)
+(assert/equal (if (> 3 2)
+                  (- 3 2)
+                  (+ 3 2))
+              1)
+
+(define x 2)
+(assert/equal (+ x 1)  3)
+(set! x 4)
+(assert/equal (+ x 1) 5)
+
 ; Pairs and Lists section, from R5RS spec
 (define x (list 'a 'b 'c))
 (define y x)
@@ -38,6 +82,41 @@
 (assert/equal (pair? '(a b c)) #t)
 (assert/equal (pair? '()) #f)
 (assert/equal (pair? '#(a b)) #f)
+
+
+(assert/equal (and (= 2 2) (> 2 1))   #t)
+(assert/equal (and (= 2 2) (< 2 1))   #f)
+;TODO: test from spec
+;(assert/equal (and 1 2 'c '(f g))     '(f g))
+(assert/equal (and)                   #t)
+
+(assert/equal (or (= 2 2) (> 2 1))   #t)
+(assert/equal (or (= 2 2) (< 2 1))   #t)
+(assert/equal (or #f #f #f)          #f)
+; TODO: test from spec
+;(assert/equal (or (memq 'b '(a b c)) 
+;                  (/ 3 0)) 
+;              '(b c))
+
+(assert/equal 
+    (let ((x 2) (y 3))
+          (* x y))
+    6)
+
+(assert/equal 
+(let ((x 2) (y 3))
+    (let ((x 7)
+                  (z (+ x y)))
+          (* z x))) 
+ 35)
+
+(assert/equal 
+(let ((x 2) (y 3))
+    (let* ((x 7)
+                    (z (+ x y)))
+          (* z x)))
+ 70)
+
 
 
 ; General tests
