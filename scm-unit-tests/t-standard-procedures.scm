32b15b75068d3f62e7c6eed03e31b4581c4465e9
justin <justin.ethier@gmail.com>
2011-01-18 22:50:26 -0500

    Added comment about where to pull these tests from...

--- scm-unit-tests/t-standard-procedures.scm
+++ scm-unit-tests/t-standard-procedures.scm
@@ -1,5 +1,8 @@
 (load "skim-unit.scm")
 
+; Adding test cases from:
+; http://www.schemers.org/Documents/Standards/R5RS/HTML/r5rs-Z-H-9.html#%_chap_6
+;
 (assert/equal
   (string=? (symbol->string obj1)
             (symbol->string obj2))
