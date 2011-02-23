059430acbf46af3b3d59066042f8f1fee2583ca5
justin <justin.ethier@gmail.com>
2011-02-23 13:25:08 -0500

    Echo file input

--- examples/simple-file-io.scm
+++ examples/simple-file-io.scm
@@ -8,8 +8,8 @@
 ;;; TODO: do something interesting like summing up all integers in a file
 ;;;
 (define a (open-input-file "examples/simple-file-io.txt"))
-(read a)
-(read a) ; TODO: need to return EOF to program instead of crashing...
+(write (read a))
+(write (read a)) ; TODO: need to return EOF to program instead of crashing...
 
 ;See (eof-object? obj) in the spec - a new object of this type must be returned when EOF is detected
 
