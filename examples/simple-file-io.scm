e1d7d72f20ad9a75eb7a6873c04471761a80fbf9
justin <justin.ethier@gmail.com>
2011-02-22 22:46:55 -0500

    Added more to example program

--- examples/simple-file-io.scm
+++ examples/simple-file-io.scm
@@ -5,6 +5,9 @@
 ;;; Sample program for file I/O
 ;;; still needs work
 ;;;
+;;; TODO: do something interesting like summing up all integers in a file
+;;;
 (define a (open-input-file "examples/simple-file-io.txt"))
 (read a)
-(read a)
+(read a) ; TODO: need to return EOF to program instead of crashing...
+(close-input-port a)
