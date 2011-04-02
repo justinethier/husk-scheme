c3eafccbead4fc863f03946ece39417bf4cd8fd4
justin <justin.ethier@gmail.com>
2011-04-02 13:45:45 -0400

    Initial file

---
+++ scm-unit-tests/t-eval.scm
@@ -0,0 +1,18 @@
+;
+; husk-scheme
+; http://github.com/justinethier/husk-scheme
+;
+; Written by Justin Ethier
+;
+; Test cases for eval 
+;
+(load "skim-unit.scm")
+
+(assert/equal (eval '(* 7 3))
+              21)
+
+(assert/equal (let ((f (eval '(lambda (f x) (f x x)))))
+                    (f + 10))
+              20)
+
+(unit-test-handler-results)
