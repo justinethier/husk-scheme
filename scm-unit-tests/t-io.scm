bca3c7f10d78149062f0b0cd47e11cc09a1c1356
justin <justin.ethier@gmail.com>
2011-04-04 23:08:45 -0400

    Initial file

---
+++ scm-unit-tests/t-io.scm
@@ -0,0 +1,13 @@
+;
+; husk-scheme
+; http://github.com/justinethier/husk-scheme
+;
+; Written by Justin Ethier
+;
+; Test cases for I/O-related forms 
+;
+(load "skim-unit.scm")
+
+; TODO: tests
+
+(unit-test-handler-results)
