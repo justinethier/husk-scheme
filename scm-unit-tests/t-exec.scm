56db23e1e9f30999f519a1014e851de57b70582b
justin <justin.ethier@gmail.com>
2011-04-15 05:07:49 -0400

    Added a test case for (dynamic-wind)

--- scm-unit-tests/t-exec.scm
+++ scm-unit-tests/t-exec.scm
@@ -31,4 +31,22 @@
                   (f (apply g args)))))
 (assert/equal ((compose sqrt *) 12 75) 30.0)
 
+(assert/equal
+ (let ((path '())
+      (c #f))
+  (let ((add (lambda (s)
+               (set! path (cons s path)))))
+    (dynamic-wind
+      (lambda () (add 'connect))
+      (lambda ()
+        (add (call-with-current-continuation
+               (lambda (c0)
+                 (set! c c0)
+                 'talk1))))
+      (lambda () (add 'disconnect)))
+    (if (< (length path) 4)
+        (c 'talk2)
+        (reverse path))))
+ '(connect talk1 disconnect connect talk2 disconnect))
+
 (unit-test-handler-results)
