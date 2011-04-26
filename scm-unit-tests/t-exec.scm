5af2b0217886b7cd9e858f3329806cd45e637b2b
justin <justin.ethier@gmail.com>
2011-04-25 22:52:34 -0400

    Added additional test cases

--- scm-unit-tests/t-exec.scm
+++ scm-unit-tests/t-exec.scm
@@ -49,4 +49,40 @@
         (reverse path))))
  '(connect talk1 disconnect connect talk2 disconnect))
 
+(assert/equal
+  (let ((path '())
+      (c #f))
+  (let ((add (lambda (s)
+               (set! path (cons s path)))))
+    (dynamic-wind
+      (lambda () (add 'connect))
+      (lambda () (add 'talk1))
+      (lambda ()
+        (add (call-with-current-continuation
+               (lambda (c0)
+                 (set! c c0)
+                 'disconnect)))))
+    (if (< (length path) 4)
+        (c 'talk2)
+        (reverse path))))
+ '(connect talk1 disconnect talk2)) ; Continuation not invoked during dynamic extent, so before/after are not invoked
+
+(assert/equal
+  (let ((path '())
+      (c #f))
+  (let ((add (lambda (s)
+               (set! path (cons s path)))))
+    (dynamic-wind
+      (lambda ()
+        (add (call-with-current-continuation
+               (lambda (c0)
+                 (set! c c0)
+                 'connect))))
+      (lambda () (add 'talk1))
+      (lambda () (add 'disconnect)))
+    (if (< (length path) 4)
+        (c 'talk2)
+        (reverse path))))
+ '(connect talk1 disconnect talk2 talk1 disconnect)) ; Because continuation captured during before, thunk/after are called twice
+
 (unit-test-handler-results)
