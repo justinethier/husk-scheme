ee20317a39a3f002030767cdba1d288de3a7079a
justin <justin.ethier@gmail.com>
2011-05-12 05:25:04 -0400

    Initial file

---
+++ hs-src/ffi-test.hs
@@ -0,0 +1,12 @@
+-- This is just a test file, but the idea is that this would be a plugin
+-- file that could be loaded at runtime by the husk interpreter.
+--
+-- This is just a concept right now, but is where we want to head with the FFI
+module Test where
+
+import Language.Scheme.Types
+--import Language.Scheme.Variables
+import Control.Monad.Error
+
+test :: [LispVal] -> IOThrowsError LispVal
+test _ = throwError $ Default "test method"
