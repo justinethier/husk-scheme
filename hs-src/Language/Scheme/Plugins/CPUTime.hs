73f983167183a360ff58e09f34531c63cfe3313a
Justin Ethier <justin@ubuntu.(none)>
2011-05-25 21:26:33 -0700

    Cleaned up the module, added a comment header.

--- hs-src/Language/Scheme/Plugins/CPUTime.hs
+++ hs-src/Language/Scheme/Plugins/CPUTime.hs
@@ -1,18 +1,39 @@
+{- | 
+Module      : Language.Scheme.Plugins.CPUTime
+Copyright   : Justin Ethier
+Licence     : MIT (see LICENSE in the distribution)
+
+Maintainer  : github.com/justinethier
+Stability   : experimental
+Portability : portable
+
+husk scheme interpreter
+
+A lightweight dialect of R5RS scheme.
+
+This module wraps System.CPUTime so that it can be used directly by Scheme code.
+
+More importantly, it serves as an example of how to wrap existing Haskell code so
+that it can be loaded and called by husk.
+
+See 'examples/ffi/ffi-cputime.scm' in the husk source tree for an example of how to
+call into this module from Scheme code.
+-}
+
 module Language.Scheme.Plugins.CPUTime (get, precision) where
 
 import Language.Scheme.Types
 import System.CPUTime
 import Control.Monad.Error
 
--- TODO: this is not really elapsed time, rather the amount of CPU time consumed...
 get, precision :: [LispVal] -> IOThrowsError LispVal
 
 {- |Wrapper for CPUTime.getCPUTime -}
 get [] = do
   t <- liftIO $ System.CPUTime.getCPUTime
   return $ Number t
-get _ = throwError $ Default "unknown args passed to test method"
+get badArgList = throwError $ NumArgs 0 badArgList
 
 {- |Wrapper for CPUTime.cpuTimePrecision -}
 precision [] = return $ Number $ System.CPUTime.cpuTimePrecision 
-precision _ = throwError $ Default "unknown args passed to test method"
+precision badArgList = throwError $ NumArgs 0 badArgList
