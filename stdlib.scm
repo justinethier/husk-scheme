2d165458588feab3d73d8a6e091c3dfca90ef21f
Justin Ethier <justin.ethier@gmail.com>
2011-07-06 21:16:55 -0700

    Use a modified version of the (and) / (or) macros from R5RS.

--- stdlib.scm
+++ stdlib.scm
@@ -68,11 +68,26 @@
 
 (define (sum . lst)     (fold + 0 lst))
 (define (product . lst) (fold * 1 lst))
-(define (and . lst)     (fold && #t lst))
-(define (or . lst)      (fold || #f lst))
 
-; TODO, forms from R5RS:
+(define-syntax and
+  (syntax-rules ()
+    ((and) (begin #t))
+    ((and test) (begin test))
+    ((and test1 test2 ...)
+     (if test1 (and test2 ...) #f))))
 
+(define-syntax or
+  (syntax-rules ()
+    ((or) (begin #f))
+    ((or test) (begin test))
+    ((or test1 test2 ...)
+     (let ((x test1))
+       (if x x (or test2 ...))))))
+
+; Issue #6
+; TODO, these are the actual forms from R5RS, and the ones we want to (eventually) use:
+; Issue is the macro code expects the pattern is a list, see above forms currently in use
+;
 ;(define-syntax and
 ;  (syntax-rules ()
 ;    ((and) #t)
