a4e27c51eb7e7e6e95f8d2338bca21f76a2354c0
Justin Ethier <justin.ethier@gmail.com>
2011-08-11 20:08:27 -0700

    Added a stub of the official "do" macro

--- stdlib.scm
+++ stdlib.scm
@@ -256,7 +256,30 @@
                   (if (null? (cdr (list var . step))) 
                       (car  (list var . step))
                       (cadr (list var . step))) ...)))))))
-
+#| TODO: this is the macro from R5RS
+(define-syntax do
+  (syntax-rules ()
+    ((do ((var init step ...) ...)
+         (test expr ...)
+         command ...)
+     (letrec
+       ((loop
+         (lambda (var ...)
+           (if test
+               (begin
+                 (if #f #f)
+                 expr ...)
+               (begin
+                 command
+                 ...
+                 (loop (do "step" var step ...)
+                       ...))))))
+       (loop init ...)))
+    ((do "step" x)
+     x)
+    ((do "step" x y)
+     y)))
+|#
 ; Delayed evaluation functions
 (define force
     (lambda (object)
