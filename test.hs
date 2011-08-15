b5e1e12795d180dc216fb9066d978683ec009033
justin <justin.ethier@gmail.com>
2011-08-15 18:00:28 -0400

    Initial file

---
+++ test.hs
@@ -0,0 +1,6 @@
+{- A temporary file to test out ideas for building the data structure required by Macro.hs -}
+
+setData :: [[String]] -> [Integer] -> String -> [[String]]
+setData dStruct ellipsisIndex val = do
+  -- TODO
+  val
