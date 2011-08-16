5f97a7aec3b53eea0497753d2ffc993bbd587d52
justin <justin.ethier@gmail.com>
2011-08-16 13:26:18 -0400

    ...

--- test.hs
+++ test.hs
@@ -3,20 +3,36 @@
  test via: ghci test.hs
 -}
 module Test (setData) where
---import Language.Scheme.Typesi
+import Control.Exception
+import Debug.Trace
 
-setData :: [a] -> [Int] -> a -> [a]
+--setData :: [a] -> [Int] -> a -> [a]
 setData dStruct ellipsisIndex@(i:is) val = do
 -- TODO: we want to break list into 3 parts - l:l[eIdx][level]:ls
-  let content = splitAt (i - 1) dStruct
+  let content = splitAt (i) dStruct
+--  case (snd (trace ("content = " ++ show content) content)) of
   case (snd content) of
---    [] ->
-    [c] -> (fst content) ++ (setData c is val)
-    (c:cs) -> (fst content) ++ (setData c is val) ++ (cs) 
+    [] -> [val]
+    [c] -> (fst content) ++ (setData [c] is val)
+    (c:cs) -> (fst content) ++ (setData [c] is val) ++ (cs) 
   dStruct ++ [val]  
 setData dStruct [] _ = dStruct
 
+
+-- TODO: focus on this section and get a set of test cases that can be run
+test input expected = do
+  putStrLn $ show input
+  putStrLn $ show $ assert (input == expected) input
+
 main :: IO ()
 main = do
-  putStr $ show $ setData [[1, 2], [3, 4, 5]] [2, 3] [6]
---  putStr $ show $ setData [[1], [2], [3], [4]] [4] [5]
+  test (setData [[1], [2], [3], [4]] [4] [5]) 
+                [[1], [2], [3], [4], [5]]
+
+  test (setData [[1, 2], [3, 4, 5]] [1, 3] [6]) 
+                [[1, 2], [3, 4, 5, 6]]
+{-
+  putStrLn $ show $ setData [[1, 2], [3, 4, 5]] [2, 3] [6]
+  putStrLn $ show $ setData [[1, 2], [3, 4, 5]] [2, 2] [6]
+  putStrLn $ show $ setData [[1, 2], [3, 4, 5]] [2, 1] [6]
+-}
