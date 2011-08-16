3b59b980f94e16358e1eb6ce6ca8b0d87b6f277b
Justin Ethier <justin.ethier@gmail.com>
2011-08-15 21:49:57 -0700

    More changes...

--- test.hs
+++ test.hs
@@ -1,14 +1,22 @@
-{- A temporary file to test out ideas for building the data structure required by Macro.hs -}
+{- A temporary file to test out ideas for building the data structure required by Macro.hs 
+
+ test via: ghci test.hs
+-}
 module Test (setData) where
 --import Language.Scheme.Typesi
 
-setData :: [a] -> [Integer] -> a -> [a]
-setData dStruct ellipsisIndex@(i:is) val level = do
+setData :: [a] -> [Int] -> a -> [a]
+setData dStruct ellipsisIndex@(i:is) val = do
 -- TODO: we want to break list into 3 parts - l:l[eIdx][level]:ls
-  content <- splitAt 
+  let content = splitAt (i - 1) dStruct
+  case (snd content) of
+--    [] ->
+    [c] -> (fst content) ++ (setData c is val)
+    (c:cs) -> (fst content) ++ (setData c is val) ++ (cs) 
   dStruct ++ [val]  
+setData dStruct [] _ = dStruct
 
 main :: IO ()
 main = do
-  putStr $ show $ setData [[1, 2], [3, 4, 5]] [2, 3] [6] 0
+  putStr $ show $ setData [[1, 2], [3, 4, 5]] [2, 3] [6]
 --  putStr $ show $ setData [[1], [2], [3], [4]] [4] [5]
