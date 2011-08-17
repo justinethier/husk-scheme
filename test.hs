a350c58c5cc486ce9e2778fd3f9a3beafdc32808
justin <justin.ethier@gmail.com>
2011-08-17 12:19:38 -0400

    Significant improvements

--- test.hs
+++ test.hs
@@ -7,43 +7,76 @@ import Language.Scheme.Types
 import Control.Exception
 import Debug.Trace
 
---setData :: [a] -> [Int] -> a -> [a]
+-- |Create a nested list
+create :: Int     -- ^ Number of nesting levels
+       -> LispVal -- ^ Empty nested list
+create level 
+    | level < 1     = Nil "" -- Error
+    | level == 1    = List []
+    | otherwise = List [create $ level - 1]
+
+-- TODO: how to handle case when requested insertion leaves "holes".
+--  for example, in a 2-level nested list: ((1)) we have data as pos 0 but have none at pos 1.
+--  if the code then tries to add an element 2 at pos 2 we should end up with:
+--
+--    ((1) () (2))
+--
+--  The current code is not this smart, but it needs to be becuase that is exactly how macros
+--  work.
+
+-- |Add an element to the given nested list
+setData :: LispVal -- ^ The nested list to modify
+        -> [Int]   -- ^ Location to insert the new element, from top-most -> leaf 
+                   --   (EG: [1, 2] means add to the second top-most list, at its 3rd position)
+        -> LispVal -- ^ Value to insert 
+        -> LispVal -- ^ Resulant list
 setData (List dStruct) ellipsisIndex@(i:is) val = do
--- TODO: we want to break list into 3 parts - l:l[eIdx][level]:ls
-  let content = splitAt (i) dStruct
-  case (snd (trace ("content = " ++ show content) content)) of
---  case (snd content) of
+  let content = splitAt i dStruct
+--  case (snd (trace ("content = " ++ show content) content)) of
+  case (snd content) of
     [] -> List $ dStruct ++ [val]
-    -- TODO: problem here is that lists are not homogenous; they consist of both [1] and 1.
-    --  this should be solved by using lispval's as list nodes, since those can contain lists and scalars.
-    --  try this and see if it will work
-    --  TBD: will it break the assertions, though? should be able to make those work with the eqv function
-    [c] ->    List $ (fst content) ++ [setData c is val]
-    (c:cs) -> List $ (fst content) ++ [setData c is val] ++ (cs) 
---  where extractList (List a) = a
-setData dStruct [] _ = dStruct
+    [c] ->    if length is < 1
+                 then List $ (fst content) ++ [val] ++ [c] -- Base case - Requested pos must be one less than c
+                 else List $ (fst content) ++ [setData c is val]
+    (c:cs) -> if length is < 1
+                 then List $ (fst content) ++ [val] ++ [c] ++ (cs) -- Base case - Requested pos must be one less than c
+                 else List $ (fst content) ++ [setData c is val] ++ (cs) 
 
-
--- TODO: focus on this section and get a set of test cases that can be run
-test input expected = do
+-- |Compare actual input with expected
+cmp input expected = do
   putStrLn $ show input
   putStrLn $ show $ assert (eqVal expected input) input
 
-main :: IO ()
-main = do
-  test (setData (List [Number 1, Number 2, Number 3, Number 4]) [4] (Number 5)) 
-                (List [Number 1, Number 2, Number 3, Number 4, Number 5])
+-- |Run this method to test the above code
+test :: IO ()
+test = do
+  cmp (setData (List [Number 1, Number 2, Number 3, Number 4]) [4] (Number 5)) 
+               (List [Number 1, Number 2, Number 3, Number 4, Number 5])
 
-  test (setData (List [List [Number 1, Number 2], List [Number 3, Number 4, Number 5]]) [1, 3] (Number 6)) 
-                (List [List [Number 1, Number 2], List [Number 3, Number 4, Number 5, Number 6]])
+  cmp (setData (List [Number 1, Number 2, Number 3, Number 4]) [1] (Number 5)) 
+               (List [Number 1, Number 5, Number 2, Number 3, Number 4])
 
---  test (setData [[1, 2], [3, 4, 5]] [1, 3] [6]) 
---                [[1, 2], [3, 4, 5, 6]]
+  cmp (setData (List [List [Number 1, Number 2], List [Number 3, Number 4, Number 5]]) [1, 3] (Number 6)) 
+               (List [List [Number 1, Number 2], List [Number 3, Number 4, Number 5, Number 6]])
 
-{-
-  test (setData [[1, 2], [3, 4, 5]] [1, 2] [6]) 
-                [[1, 2], [3, 4, 6, 5]]
+  cmp (setData (List [List [Number 1, Number 2], List [Number 3, Number 4, Number 5]]) [1, 2] (Number 6)) 
+               (List [List [Number 1, Number 2], List [Number 3, Number 4, Number 6, Number 5]])
 
-  test (setData [[1, 2], [3, 4, 5]] [0, 2] [6]) 
-                [[1, 2, 6], [3, 4, 5]]
-                -}
+  cmp (setData (List [List [Number 1, Number 2], List [Number 3, Number 4, Number 5]]) [0, 2] (Number 6)) 
+               (List [List [Number 1, Number 2, Number 6], List [Number 3, Number 4, Number 5]])
+
+  let a = create 2
+  cmp a 
+      (List [List []])
+
+  let b = setData a [0, 0] $ Atom "test"
+  cmp b (List [List [Atom "test"]])
+
+  let c = setData b [0, 1] $ Atom "test2"
+  cmp c (List [List [Atom "test", Atom "test2"]])
+
+-- TODO: Illustrates an important point, that if we are adding into a 'hole', we need to create a list there first
+  let cc = setData b [1, 0] $ Atom "test2"
+  cmp cc (List [List [Atom "test"], List [Atom "test2"]])
+
+
