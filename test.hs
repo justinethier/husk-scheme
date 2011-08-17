04ec85b2628f259198913d1c1f13d360a5660610
Justin Ethier <justin.ethier@gmail.com>
2011-08-16 22:39:49 -0700

    Added a TODO

--- test.hs
+++ test.hs
@@ -3,6 +3,7 @@
  test via: ghci test.hs
 -}
 module Test (setData) where
+import Language.Scheme.Types
 import Control.Exception
 import Debug.Trace
 
@@ -10,12 +11,15 @@ import Debug.Trace
 setData dStruct ellipsisIndex@(i:is) val = do
 -- TODO: we want to break list into 3 parts - l:l[eIdx][level]:ls
   let content = splitAt (i) dStruct
---  case (snd (trace ("content = " ++ show content) content)) of
-  case (snd content) of
-    [] -> [val]
-    [c] -> (fst content) ++ (setData [c] is val)
+  case (snd (trace ("content = " ++ show content) content)) of
+--  case (snd content) of
+    [] -> dStruct ++ [val]
+    -- TODO: problem here is that lists are not homogenous; they consist of both [1] and 1.
+    --  this should be solved by using lispval's as list nodes, since those can contain lists and scalars.
+    --  try this and see if it will work
+    --  TBD: will it break the assertions, though? should be able to make those work with the eqv function
+    [[c]] -> (fst content) ++ (setData [c] is val)
     (c:cs) -> (fst content) ++ (setData [c] is val) ++ (cs) 
-  dStruct ++ [val]  
 setData dStruct [] _ = dStruct
 
 
@@ -31,8 +35,11 @@ main = do
 
   test (setData [[1, 2], [3, 4, 5]] [1, 3] [6]) 
                 [[1, 2], [3, 4, 5, 6]]
+
 {-
-  putStrLn $ show $ setData [[1, 2], [3, 4, 5]] [2, 3] [6]
-  putStrLn $ show $ setData [[1, 2], [3, 4, 5]] [2, 2] [6]
-  putStrLn $ show $ setData [[1, 2], [3, 4, 5]] [2, 1] [6]
--}
+  test (setData [[1, 2], [3, 4, 5]] [1, 2] [6]) 
+                [[1, 2], [3, 4, 6, 5]]
+
+  test (setData [[1, 2], [3, 4, 5]] [0, 2] [6]) 
+                [[1, 2, 6], [3, 4, 5]]
+                -}
