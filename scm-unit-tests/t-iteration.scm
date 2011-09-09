3750664f2a886cf817cb284bca4d89c2e6411b14
Justin Ethier <justin.ethier@gmail.com>
2011-09-09 07:15:18 -0700

    Temporarily removing a test case, added a TODO

--- scm-unit-tests/t-iteration.scm
+++ scm-unit-tests/t-iteration.scm
@@ -16,6 +16,14 @@
                      (vector-set! vec i i))
                 '#(0 1 2 3 4))
 
+(assert/equal #t #f) ;; Adding this as a reminder for below
+#|
+TODO: this case is going into an infinite loop. However, it seems that the
+issue is not with the macro code. Or at least, if it is the problem is within
+named let or let*. Anyway, I am commenting it out for now to focus on getting the
+rest of the code working after the latest macro changes. Once this branch is in
+a better state I will revisit this issue...
+
 ; Per spec, <step> does not have to be specified
 (assert/equal 
                 (do ((vec (make-vector 5))
@@ -23,7 +31,7 @@
                     ((= i 5) vec)
                      (vector-set! vec i i))
                 '#(0 1 2 3 4))
-
+|#
 (assert/equal 
                 (let ((x '(1 3 5 7 9)))
                     (do ((x x (cdr x))
