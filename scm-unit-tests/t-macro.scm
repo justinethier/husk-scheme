722e56693dc3a6338c03793ca121c56baf54c29e
justin <justin.ethier@gmail.com>
2011-09-12 10:06:19 -0400

    Added two more test cases

--- scm-unit-tests/t-macro.scm
+++ scm-unit-tests/t-macro.scm
@@ -228,6 +228,13 @@
   (my-pair-test/04 (1 2) (3 4 5))
   '((1 2) (3 4 5)))
 
+(assert/equal
+  (my-pair-test/04 (1 2) (3 4 5) (6 7) (8 9 10))
+  '((1 2) (3 4 5) (6 7) (8 9 10)))
+
+(assert/equal
+  (my-pair-test/04 (1 2) (3 4 5) (6 7) (8 9 10) (11 12))
+  '((1 2) (3 4 5) (6 7) (8 9 10) (11 12)))
 
 (define-syntax my-pair-test/05
   (syntax-rules ()
