65545ce9196759e0aad4c383901f31af99d74a02
justin <justin.ethier@gmail.com>
2011-10-20 12:46:03 -0400

    Simplified the test case..

--- lambda-test-cases.scm
+++ lambda-test-cases.scm
@@ -3,8 +3,11 @@
 ;(write
 ;  (let ((z 1)) ((lambda () z))))
 ;
-(write
-  (let ((x 2) (y 3)) (let ((x 7) (z (+ x y))) (* z x))))
+;(write
+;  (let ((x 2) (y 3)) (let ((x 7) (z (+ x y))) (* z x))))
+
+(write (let ((x 2) (y 3)) (+ x y)))
+
 ;
 ;(write (let ((name 'a)) `(list ,name . ,name)))
 ;(write (let ((name 'a)) `(list 1 . 1)))
