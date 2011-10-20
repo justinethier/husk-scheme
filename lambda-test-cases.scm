82d8bf3cab601833895ffdea601107fe4d3c0456
justin <justin.ethier@gmail.com>
2011-10-20 13:16:23 -0400

    Added some notes regarding the test failure

--- lambda-test-cases.scm
+++ lambda-test-cases.scm
@@ -1,6 +1,16 @@
 ; test cases for hygienic macro rework
 
+; Following test case does not work, output below:
+;
+;a = let t = (let ((x 7)) x1) ex = {(let (((x1 y2) 7)) x1), True, 1} expanded = ((lambda () x1) 7)
+;
+;some initial thoughts:
+;
+;perhaps each recursively expanded macro needs its own environment, to prevent the outer x from
+;clobbering x in the inner macro.
 (write (let ((x 2) (y 3)) (let ((x 7)) x)))
+
+
 
 (write
   (let ((z 1)) ((lambda () z))))
