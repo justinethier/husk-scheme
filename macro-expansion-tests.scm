081e12e511b4813b1620e12ca1c335164203a026
Justin Ethier <justin.ethier@gmail.com>
2011-10-29 14:27:38 -0700

    Migrated a failing test over here to examine it more closely

--- macro-expansion-tests.scm
+++ macro-expansion-tests.scm
@@ -20,3 +20,14 @@
 )
 ;    4)
 ;=> #f instead of 4
+
+; Assert that a template can be quoted, allowing someone debugging a macro the
+; ability to see the expansion of that macro
+(define-syntax orr-debugging 
+  (syntax-rules () 
+   ((orelse <expr1> <expr2>) 
+    '(let ((temp <expr1>)) 
+         (if temp temp <expr2>)))))
+
+(write (orr-debugging 1 1)
+              '(let ((temp 1)) (if temp temp 1)))
