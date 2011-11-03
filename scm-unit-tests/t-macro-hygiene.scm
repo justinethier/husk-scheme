b72ea8e60a7c1d18f38c30c1fde974189302b924
Justin Ethier <justin.ethier@gmail.com>
2011-11-03 01:59:31 -0700

    Working through the test suite

--- scm-unit-tests/t-macro-hygiene.scm
+++ scm-unit-tests/t-macro-hygiene.scm
@@ -1,18 +1,39 @@
+;(define-syntax a
+; (syntax-rules ()
 
+
+; if marked as a literal identifier so it is not loaded as a pattern variable...
 (define-syntax test
- (syntax-rules ()
+ (syntax-rules (if)
    ((_ if)
-    (if #t #t #f))))
+    (if #t #t #f))
+   ((_ a)
+    'no-match)))
 
-; Appears func is called here...
-; presumably because the let's if is not in the env when test is expanded?
+; if is introduced by (let) so it should not match in the macro because
+; it is defined differently in env(use) and env(def):
 (write
   (let ((if (lambda (x y z) "func is called"))) (test if)))
+
+; But here there is no definition in env(use) so it does match the first rule
+(write
+  (test if))
+
 
 ; In this case, if should be taken from the def environment (IE, the special form) and
 ; not from the use environment (which is the "should not happen" function.
 ;
 ; BUT, I'm confused, why does the macro match at all?
 (define if (lambda (x y z) "should not happen"))
+;(define if 1)
 (write
   (test if))
+
+; Since the new if is now in the def environment, it should execute the function this time
+(define-syntax test-2
+ (syntax-rules (if)
+   ((_ if)
+    (if #t #t #f))))
+(write
+  (test-2 if))
+
