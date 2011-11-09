75055b94baa80c20fa2b2fc99f397ddfbc381206
justin <justin.ethier@gmail.com>
2011-11-09 15:39:41 -0500

    Added lambda form back...

--- mhtest.scm
+++ mhtest.scm
@@ -1,19 +1,5 @@
-; expressions from Issue 56
-; mangles results: (letrec ((z 12)) z (let () (let ((z 1) (y 2)) (+ z y))) z)
-
-;(letrec ((z 12)) 1 2 z)
-(letrec ((z 12)) (let () (let ((z 1) (y 2)) (+ z y))) z)
-#|
-; husk expansion
-; looks like the z parameter is lost somehow
-((lambda () (define z 12) ((lambda () ((lambda () ((lambda (z7 y8) (+ z7 y8)) 1 2)))))))
-; chicken expansion
-(##core#let () (define50 z 12) (let49 () (let () (let ((z 1) (y 2)) (+ z y))) z))
-|#
-
-#|
 ; Lot of weird things going on here...
-;((lambda ()
+((lambda ()
 ;(let ()
   (define var-02 1)
   (define-syntax test-template
@@ -28,4 +14,4 @@
   (define var-02 3)
   (write (test-template))
 ))
-|#
+))
