4449dfb5207451377033b81d3e9ca4b9ca672748
justin <justin.ethier@gmail.com>
2011-11-10 06:30:17 -0500

    More test cases

--- scm-unit-tests/t-macro-ref-trans.scm
+++ scm-unit-tests/t-macro-ref-trans.scm
@@ -26,4 +26,35 @@
     (assert/equal (test-template) 3)))) 
   1))
 
+(let ()
+  (define var-02 1)
+  (define-syntax test-template
+   (syntax-rules ()
+     ((_)
+      var-02)))
+  
+  ((lambda ()
+    (assert/equal (test-template) 1)
+    (assert/equal (let ((var-02 1)) (test-template)) 1)
+    (assert/equal (let ((var-02 2)) (test-template)) 1)
+    (define var-02 3)
+    (assert/equal (test-template) 3)))) 
+
+; Variable defined within macro (1) should take precedence over
+; the one in the outer env (33)
+(define var-02 33)
+(let ()
+  (define var-02 1)
+  (define-syntax test-template
+   (syntax-rules ()
+     ((_)
+      var-02)))
+  
+  ((lambda ()
+    (assert/equal (test-template) 1)
+    (assert/equal (let ((var-02 1)) (test-template)) 1)
+    (assert/equal (let ((var-02 2)) (test-template)) 1)
+    (define var-02 3)
+    (assert/equal (test-template) 3)))) 
+
 (unit-test-handler-results)
