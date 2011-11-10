88034fb31aacdd994f562fee614398f3bc14165a
justin <justin.ethier@gmail.com>
2011-11-10 06:16:58 -0500

    Initial file

---
+++ scm-unit-tests/t-macro-ref-trans.scm
@@ -0,0 +1,29 @@
+;;
+;; husk-scheme
+;; http://github.com/justinethier/husk-scheme
+;;
+;; Written by Justin Ethier
+;;
+;; Test cases specifically for referential transparency.
+;;
+(load "skim-unit.scm")
+(unit-test-start "hygienic macros - referential transparency")
+
+; Testing ability of a nested macro def to use a renamed var
+(let ()
+ ((lambda (var-02)
+
+  (define-syntax test-template
+   (syntax-rules ()
+     ((_)
+      var-02)))
+  
+  ((lambda ()
+    (assert/equal (test-template) 1)
+    (assert/equal (let ((var-02 1)) (test-template)) 1)
+    (assert/equal (let ((var-02 2)) (test-template)) 1)
+    (define var-02 3)
+    (assert/equal (test-template) 3)))) 
+  1))
+
+(unit-test-handler-results)
