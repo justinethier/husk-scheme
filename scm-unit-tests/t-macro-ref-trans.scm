daf044e29fdf3fa549f7433ae473621b12a9736e
justin <justin.ethier@gmail.com>
2011-11-10 23:55:36 -0500

    Added test cases for set!

--- scm-unit-tests/t-macro-ref-trans.scm
+++ scm-unit-tests/t-macro-ref-trans.scm
@@ -57,4 +57,18 @@
     (define var-02 3)
     (assert/equal (test-template) 3)))) 
 
+(define var-02 33)
+(let ()
+  (set! var-02 1)
+  (define-syntax test-template
+   (syntax-rules ()
+     ((_)
+      var-02)))
+  
+  ((lambda ()
+    (assert/equal (test-template) 1)
+    (assert/equal (let ((var-02 1)) (test-template)) 1)
+    (assert/equal (let ((var-02 2)) (test-template)) 1)
+    (define var-02 3)
+    (assert/equal (test-template) 3)))) 
 (unit-test-handler-results)
