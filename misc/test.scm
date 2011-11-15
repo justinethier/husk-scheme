9b34ff3ca79eb4f7a62d76460f761c272ba10c0b
justin <justin.ethier@gmail.com>
2011-11-15 03:58:36 -0500

    Switched to the true test case from 3.3

--- misc/test.scm
+++ misc/test.scm
@@ -1,5 +1,6 @@
 ; From: 
 ; http://groups.google.com/group/comp.lang.scheme/msg/eb6cc6e11775b619?pli=1
+#| TODO: add this to the macro test suite, expected value: (1 2 3 a)
 (let ((a 1)) 
      (letrec-syntax 
          ((foo (syntax-rules () 
@@ -11,3 +12,13 @@
                             (list d c 'c))))))) 
        (let ((a 2)) 
          (foo a)))) 
+|#
+(write
+  (let ((x 1))
+    (let-syntax
+        ((foo (syntax-rules ()
+                ((_ y) (let-syntax
+                             ((bar (syntax-rules ()
+                                   ((_) (let ((x 2)) y)))))
+                         (bar))))))
+      (foo x))))
