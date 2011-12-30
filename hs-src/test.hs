a76896c39b2fcaff5725b5ecfc43a1be5fa941a7
Justin <justin@ubuntu.(none)>
2011-12-30 13:55:53 -0500

    WIP

--- hs-src/test.hs
+++ hs-src/test.hs
@@ -15,10 +15,15 @@ import System.IO
 --import System.Environment
 --import System.Console.Haskeline
 
-main :: IO LispVal --()
+main :: IO ()
 main = do
---TODO: how to handle throws, how to handle multiple expressions...
-  writeProc (\ port obj -> hPrint port obj) [numAdd [Number 1, Number 2]]
+  (runIOThrows $ liftM show $ run) >>= putStr ""  -- TODO: args  - see shell.hs
+
+run :: IOThrowsError LispVal
+run = do
+--TODO: how to handle throws, how to handle multiple expressions (hint: we need to use CPS)...
+  result <- liftThrows $ numAdd [Number 1, Number 2]
+  writeProc (\ port obj -> hPrint port obj) [result]
 {-main = do args <- getArgs
           if null args then do showBanner
                                runRepl
