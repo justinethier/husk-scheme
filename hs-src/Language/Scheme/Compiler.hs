87266140bebb5691eef08b8dbf73d47c53cc0e72
Justin <justin@ubuntu.(none)>
2011-12-30 23:56:41 -0500

    Added many TODO's and high-level notes

--- hs-src/Language/Scheme/Compiler.hs
+++ hs-src/Language/Scheme/Compiler.hs
@@ -36,3 +36,21 @@ import System.IO
 compileLisp :: String -> IO ()
 compileLisp file = do
   putStrLn $ "TODO: load file " ++ file
+
+{-
+TODO:
+
+- a function to load a scheme file
+- a function to compile a line of scheme code to haskell
+  TODO: can compilation happen one line at a time? for now that may be a good enough start
+
+  assume compilation will need to use gensym/name-mangling to emit functions and maintain a mapping of
+  previously-defined variables, in order to interleave them into the compiled program
+
+  will need to emit equivalents to continueEval, apply, and other forms from the interpreter. we will need to
+  maintain CPS within the compiled code
+
+TBD: can we compile directly to haskell, or do we need an intermediate representation?
+     maybe a direct compile is good enough for the initial proof-of-concept
+
+-}
