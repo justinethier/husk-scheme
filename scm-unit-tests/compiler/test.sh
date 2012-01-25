9d68edc3a9d5ec0c01e54d3e45793d98ef82ebc8
justin <justin.ethier@gmail.com>
2012-01-25 13:13:23 -0500

    Relocated compiler test files

--- scm-unit-tests/compiler/test.sh
+++ scm-unit-tests/compiler/test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-./huskc misc/comp-test.scm
 ghc -cpp -Wall --make -package ghc -fglasgow-exts -o test _tmp.hs hs-src/Language/Scheme/Primitives.hs hs-src/Language/Scheme/Parser.hs hs-src/Language/Scheme/Numerical.hs hs-src/Language/Scheme/Core.hs hs-src/Language/Scheme/Macro.hs hs-src/Language/Scheme/FFI.hs hs-src/Language/Scheme/Macro/Matches.hs
 ./test
