86bc3069c9636785be2bbc3b683b9b7a655f8407
Justin Ethier <justin.ethier@gmail.com>
2012-01-24 22:44:47 -0500

    Migrated all (write) operations into test cases

--- misc/comp-test.scm
+++ misc/comp-test.scm
@@ -25,55 +25,53 @@
 1
 (if 1 2 3)
 
-(define b 1) ; TODO: error when b is referenced below
-(write b)    ;  see also test1.scm, which has the same problem (but in a smaller file)
+(define b 1)
+(assert-equal 1.3 1 b)
 
-(define (a x) (write x))
-(a 1234567890)
+(define (a x) x)
+(assert-equal 1.4 1234567890 (a 1234567890))
 
 (lambda () 1)
 
-(write (((lambda () +)) 1 2 3 4))
-(write ((lambda (a b c) (+ a b c)) 2 4 6))
-(write (if #f #t #f))
-(write (if ((lambda () #t)) (+ 100) (+ 200)))
-(write (if ((lambda () #f)) (+ 100) (+ 200)))
+(assert-equal 1.5 10  (((lambda () +)) 1 2 3 4))
+(assert-equal 1.6 12  ((lambda (a b c) (+ a b c)) 2 4 6))
+(assert-equal 1.7 #f  (if #f #t #f))
+(assert-equal 1.8 100 (if ((lambda () #t)) (+ 100) (+ 200)))
+(assert-equal 1.9 200 (if ((lambda () #f)) (+ 100) (+ 200)))
 1
 2
-;(write (+ 11 22 33))
-(write (+ 1 2 3))
-(write (+ (+ 1) (+ (+ 2 2)) (+ (+ 3 3) (+ (+ 3)))))
+(assert-equal 1.10 6 (+ 1 2 3))
+(assert-equal 1.11 14 (+ (+ 1) (+ (+ 2 2)) (+ (+ 3 3) (+ (+ 3)))))
 4
 1
 2
-(write (+ 11 22 33))
+(assert-equal 1.12 66 (+ 11 22 33))
 3
-#|
 1
 2
-(write 3)
+(assert-equal 1.13 3 3)
 2
-(write (+ 1 2 (+ 3 4 5)))
-(write (+ 1 2 (+ 3 (+ 4 6) 5)))
-(write (+ 1 (+ 2) (+) 3 (- 9 2) (/ 9 3)))
-(write (+ 1 (+ 2) 3 (- 9 2) (/ 9 3)))
-(write (+ (+ 2) 3 (- 9 2) (/ 9 3)))
-(write (+ 3 (- 9 2) (/ 9 3)))
-(write (+ (- 9 2) (/ 9 3)))
-(write (+ (/ 9 3))) ; results are OK
-(write (if (+ 1 (+ 2) (+) 3 (- 9 2) (/ 9 3)) 
+(assert-equal 1.14 15 (+ 1 2 (+ 3 4 5)))
+(assert-equal 1.15 21 (+ 1 2 (+ 3 (+ 4 6) 5)))
+(assert-equal 1.16 16 (+ 1 (+ 2) (+) 3 (- 9 2) (/ 9 3)))
+(assert-equal 1.17 16 (+ 1 (+ 2) 3 (- 9 2) (/ 9 3)))
+(assert-equal 1.18 15 (+ (+ 2) 3 (- 9 2) (/ 9 3)))
+(assert-equal 1.19 13 (+ 3 (- 9 2) (/ 9 3)))
+(assert-equal 1.20 10 (+ (- 9 2) (/ 9 3)))
+(assert-equal 1.21 3 (+ (/ 9 3))) ; results are OK
+(assert-equal 1.22 16 (if (+ 1 (+ 2) (+) 3 (- 9 2) (/ 9 3)) 
            (+ 1 (+ 2) (+) 3 (- 9 2) (/ 9 3))
            (+ 1 (+ 2) (+) 3 (- 9 2) (/ 9 3))))
-(write (if 1 (+ 2 1) (+ 3 1)))
-(write (if 1 (+ 2 1 4) 3))
-(write (if 1 2 3))
-(write (if 1 (+ 2 1 4) 3))
-(write (if (+ 1) 2 3))
-(write (if #f 2 #f))
-(write (if (+ 1) 22 3))
+(assert-equal 1.23 3 (if 1 (+ 2 1) (+ 3 1)))
+(assert-equal 1.24 7 (if 1 (+ 2 1 4) 3))
+(assert-equal 1.25 2 (if 1 2 3))
+(assert-equal 1.26 7 (if 1 (+ 2 1 4) 3))
+(assert-equal 1.27 2 (if (+ 1) 2 3))
+(assert-equal 1.28 #f (if #f 2 #f))
+(assert-equal 1.29 22 (if (+ 1) 22 3))
 
-(write ((lambda () 1 2 (+ 3) )))
-(write ((lambda () 1 2 3 )))
+(assert-equal 1.30 3 ((lambda () 1 2 (+ 3) )))
+(assert-equal 1.31 3 ((lambda () 1 2 3 )))
 ((lambda () 1 2 (+ 3 4)))
 ; TODO: (write (if #t (+ ((lambda () 1 2 3) 3)) 4))
-|#
+
