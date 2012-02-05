6090f9acfdf1de3f1eadd9b163e15eaeacd90a62
Justin Ethier <justin.ethier@gmail.com>
2012-02-04 23:05:07 -0500

    Removed unnecessary macro definitions

--- tests/compiler/macro.scm
+++ tests/compiler/macro.scm
@@ -1,15 +1,1 @@
-(define-syntax let
-  (syntax-rules ()
-    ((_ ((x v) ...) e1 e2 ...)
-     ((lambda (x ...) e1 e2 ...) v ...))
-    ((_ name ((x v) ...) e1 e2 ...)
-     (let*
-       ((f  (lambda (name)
-              (lambda (x ...) e1 e2 ...)))
-        (ff ((lambda (proc) (f (lambda (x ...) ((proc proc)
-               x ...))))
-             (lambda (proc) (f (lambda (x ...) ((proc proc)
-               x ...)))))))
-        (ff v ...)))))
-
 (let ((x 1)) x)
