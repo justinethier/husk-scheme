01fbae6e1855fdbacfa8105e7fcaaa4403e0f9bc
justin <justin.ethier@gmail.com>
2012-03-07 13:30:53 -0500

    Added runIOThrows' to allow only returning an error message.

--- hs-src/Language/Scheme/Types.hs
+++ hs-src/Language/Scheme/Types.hs
@@ -92,8 +92,19 @@ liftThrows :: ThrowsError a -> IOThrowsError a
 liftThrows (Left err) = throwError err
 liftThrows (Right val) = return val
 
+-- |Execute an IO action and return result or an error message.
+--  This is intended for use by a REPL, where a result is always
+--  needed regardless of type.
 runIOThrows :: IOThrowsError String -> IO String
 runIOThrows action = runErrorT (trapError action) >>= return . extractValue
+
+-- |Execute an IO action and return error or Nothing if no error was thrown.
+runIOThrows' :: IOThrowsError String -> IO (Maybe String)
+runIOThrows' action = do
+    runState <- runErrorT action
+    case runState of
+        Left err -> return $ Just (show err)
+        Right _ -> return $ Nothing
 
 -- |Scheme data types
 data LispVal = Atom String
