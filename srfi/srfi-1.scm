ad7b472b33ff45c0f9c27b77b7ee1a9c917bfdcf
justin <justin.ethier@gmail.com>
2012-04-03 16:38:53 -0400

    Added most of the predicates

--- srfi/srfi-1.scm
+++ srfi/srfi-1.scm
@@ -5,6 +5,12 @@
 ;;;
 ;;; Written by Justin Ethier
 ;;;
+;;;
+;;; Most of this code was taken from the reference implementation 
+;;; at http://srfi.schemers.org/srfi-1/srfi-1-reference.scm
+;;; Copyright (c) 1998, 1999 by Olin Shivers.
+;;;
+;;;
 ;;; Implementation of SRFI-1: List Library
 ;;;
 ;;; This file contains all of the SRFI-1 functions
@@ -17,6 +23,10 @@
 ; just take the name of the SRFI and (load) that
 ; srfi file. No need to implement full module/library
 ; semantics just yet. But this will be a first step...
+;
+; A simple way to do this is via SRFI 55's require-extension:
+; http://srfi.schemers.org/srfi-55/srfi-55.html
+;
 
 ;; Helpers (TODO: should be private to this "module")
 (define (check-arg pred val caller)
@@ -84,7 +94,71 @@
 
 
 ;; Predicates
+(define (proper-list? x)
+  (let lp ((x x) (lag x))
+    (if (pair? x)
+	(let ((x (cdr x)))
+	  (if (pair? x)
+	      (let ((x   (cdr x))
+		    (lag (cdr lag)))
+		(and (not (eq? x lag)) (lp x lag)))
+	      (null? x)))
+	(null? x))))
+
+
+;;; A dotted list is a finite list (possibly of length 0) terminated
+;;; by a non-nil value. Any non-cons, non-nil value (e.g., "foo" or 5)
+;;; is a dotted list of length 0.
+;;;
+;;; <dotted-list> ::= <non-nil,non-pair>	; Empty dotted list
+;;;               |   (cons <x> <dotted-list>)	; Proper-list pair
+
+(define (dotted-list? x)
+  (let lp ((x x) (lag x))
+    (if (pair? x)
+	(let ((x (cdr x)))
+	  (if (pair? x)
+	      (let ((x   (cdr x))
+		    (lag (cdr lag)))
+		(and (not (eq? x lag)) (lp x lag)))
+	      (not (null? x))))
+	(not (null? x)))))
+
+(define (circular-list? x)
+  (let lp ((x x) (lag x))
+    (and (pair? x)
+	 (let ((x (cdr x)))
+	   (and (pair? x)
+		(let ((x   (cdr x))
+		      (lag (cdr lag)))
+		  (or (eq? x lag) (lp x lag))))))))
+
 (define (not-pair? x) (not (pair? x)))
+
+;;; This is a legal definition which is fast and sloppy:
+;;;     (define null-list? not-pair?)
+;;; but we'll provide a more careful one:
+(define (null-list? l)
+  (cond ((pair? l) #f)
+	((null? l) #t)
+	(else (error "null-list?: argument out of domain" l))))
+           
+(define (list= = . lists)
+  (or (null? lists) ; special case
+
+      (let lp1 ((list-a (car lists)) (others (cdr lists)))
+	(or (null? others)
+	    (let ((list-b (car others))
+		  (others (cdr others)))
+	      (if (eq? list-a list-b)	; EQ? => LIST=
+		  (lp1 list-b others)
+		  (let lp2 ((list-a list-a) (list-b list-b))
+		    (if (null-list? list-a)
+			(and (null-list? list-b)
+			     (lp1 list-b others))
+			(and (not (null-list? list-b))
+			     (= (car list-a) (car list-b))
+			     (lp2 (cdr list-a) (cdr list-b)))))))))))
 
 ;; Selectors
 (define first  car)
@@ -97,3 +171,20 @@
 (define (eighth  x) (cadddr (cddddr x)))
 (define (ninth   x) (car  (cddddr (cddddr x))))
 (define (tenth   x) (cadr (cddddr (cddddr x))))
+
+
+;; Misc
+
+(define (length+ x)			; Returns #f if X is circular.
+  (let lp ((x x) (lag x) (len 0))
+    (if (pair? x)
+	(let ((x (cdr x))
+	      (len (+ len 1)))
+	  (if (pair? x)
+	      (let ((x   (cdr x))
+		    (lag (cdr lag))
+		    (len (+ len 1)))
+		(and (not (eq? x lag)) (lp x lag len)))
+	      len))
+	len)))
+
--- tests/t-srfi-1.scm
+++ tests/t-srfi-1.scm
@@ -13,11 +13,23 @@
 
 (load "../srfi/srfi-1.scm")
 
+; Constructors
 (assert/equal (cons* 1 2 3 4) '(1 2 3 . 4))
 (assert/equal (cons* 1) 1)
 (assert/equal (make-list 4 'c) '(c c c c))
 (assert/equal (list-tabulate 4 values) '(0 1 2 3))
 ; TODO: (assert/equal (iota 5) '(0 1 2 3 4))
 ; TODO: (assert/equal (iota 5 0 -0.1) '(0 -0.1 -0.2 -0.3 -0.4))
+
+; Predicates
+(assert/equal (proper-list? (list))       #t)
+(assert/equal (proper-list? '(1 . 2))      #f)
+; circular-list?
+(assert/equal (dotted-list? (list))       #f)
+(assert/equal (dotted-list? '(1 . 2))     #t)
+(assert/equal (null-list? '())        #t)
+(assert/equal (not-pair? '(1 . 2))    #f)
+(assert/equal (list= eq?)             #t)       ; Trivial cases
+(assert/equal (list= eq? '(a))        #t)
 
 (unit-test-handler-results)
