dfdd3186c92c474d7398c11b49c67b62e08db599
justin <justin.ethier@gmail.com>
2012-04-09 06:20:20 -0400

    Issue #27 - Added an initial set of tests

--- tests/t-io.scm
+++ tests/t-io.scm
@@ -6,26 +6,29 @@
 ;;
 ;; Test cases for I/O-related forms 
 ;;
-(unit-test-start "I/O (TODO)")
+(unit-test-start "I/O")
 
-; TODO: tests for the following:
-#|
-ioPrimitives = [("open-input-file", makePort ReadMode),
-                ("open-output-file", makePort WriteMode),
-                ("close-input-port", closePort),
-                ("close-output-port", closePort),
-                ("input-port?", isInputPort),
-                ("output-port?", isOutputPort),
-                ("char-ready?", isCharReady),
-                ("current-input-port", currentInputPort),
-                ("current-output-port", currentOutputPort),
-                ("read", readProc),
-                ("read-char", readCharProc hGetChar),
-                ("peek-char", readCharProc hLookAhead),
-                ("write", writeProc (\ port obj -> hPrint port obj)),
-                ("write-char", writeCharProc),
-                ("display", writeProc (\ port obj -> case obj of
-                ("read-contents", readContents),
-                ("read-all", readAll),
-                |#
+(define *filename* "_Test.txt")
+(define *list-data* '(1 2 ((3)) (4 5 6) ((((7))))))
+(define ofp (open-output-file *filename*))
+
+(assert/equal (output-port? ofp) #t)
+(assert/equal (input-port? ofp) #f)
+(assert/equal (output-port? ofp) #t)
+(assert/equal (input-port? ofp) #f)
+(write *list-data* ofp)
+(close-output-port ofp)
+;(assert/equal (output-port? ofp) #f)
+
+(define ifp (open-input-file *filename*))
+(assert/equal (output-port? ifp) #f)
+(assert/equal (input-port? ifp) #t)
+(assert/equal (char-ready? ifp) #t)
+(assert/equal (peek-char ifp) #\()
+
+(let ((data (read ifp)))
+  (assert/equal data *list-data*))
+
+(close-input-port ifp)
+
 (unit-test-handler-results)
