bb2d076385d1743d9b0f97dd779cb4afa0cb11e8
justin <justin.ethier@gmail.com>
2012-04-09 06:20:10 -0400

    Added I/O test suite

--- tests/run-tests.scm
+++ tests/run-tests.scm
@@ -28,5 +28,6 @@
 (let () (load "t-macro-hygiene.scm"))
 (let () (load "t-macro-ref-trans.scm"))
 (let () (load "t-srfi-1.scm"))
+(let () (load "t-io.scm"))
 (let () (load "summarize.scm"))
 ; TODO: @rm -f $(UNIT_TEST_DIR)/scm-unit.tmp
