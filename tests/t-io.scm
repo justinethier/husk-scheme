5db01d6d7b66762b94a82e036a338ecca9ef27fc
Justin Ethier <justin.ethier@gmail.com>
2012-04-11 23:02:17 -0400

    Added a list of functions to test

--- tests/t-io.scm
+++ tests/t-io.scm
@@ -8,6 +8,24 @@
 ;;
 (unit-test-start "I/O (TODO)")
 
-; TODO: tests
-
+; TODO: tests for the following:
+#|
+ioPrimitives = [("open-input-file", makePort ReadMode),
+                ("open-output-file", makePort WriteMode),
+                ("close-input-port", closePort),
+                ("close-output-port", closePort),
+                ("input-port?", isInputPort),
+                ("output-port?", isOutputPort),
+                ("char-ready?", isCharReady),
+                ("current-input-port", currentInputPort),
+                ("current-output-port", currentOutputPort),
+                ("read", readProc),
+                ("read-char", readCharProc hGetChar),
+                ("peek-char", readCharProc hLookAhead),
+                ("write", writeProc (\ port obj -> hPrint port obj)),
+                ("write-char", writeCharProc),
+                ("display", writeProc (\ port obj -> case obj of
+                ("read-contents", readContents),
+                ("read-all", readAll),
+                |#
 (unit-test-handler-results)
