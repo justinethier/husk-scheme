7c6c346629f38c264b50c507e67d0f90ae251622
Justin Ethier <justin.ethier@gmail.com>
2012-06-01 17:48:35 -0400

    Added more includes

--- extensions/examples/json.scm
+++ extensions/examples/json.scm
@@ -1,5 +1,9 @@
+; Dynamically load functions from the JSON module
+; This takes Haskell a few moments...
 (load-ffi "Language.Scheme.Plugins.JSON" "jsDecode" "json:decode")
+(load-ffi "Language.Scheme.Plugins.JSON" "jsDecodeStrict" "json:decode-strict")
 (load-ffi "Language.Scheme.Plugins.JSON" "jsEncode" "json:encode")
+(load-ffi "Language.Scheme.Plugins.JSON" "jsEncodeStrict" "json:encode-strict")
 
 (write (json:encode '()))
 (write (json:encode '(test 1 2 3)))
