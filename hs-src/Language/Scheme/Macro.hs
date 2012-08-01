cc35badea4ff355320515c25d4cefd825ba0347a
Justin Ethier <justin.ethier@gmail.com>
2012-07-31 22:58:13 -0400

    Refactoring

--- hs-src/Language/Scheme/Macro.hs
+++ hs-src/Language/Scheme/Macro.hs
@@ -167,55 +167,60 @@ explicitRenamingTransform ::
     -> IOThrowsError LispVal
 explicitRenamingTransform useEnv renameEnv lisp 
                             transformer@(Func _ _ _ defEnv) apply = do
+  let continuation = makeNullContinuation useEnv
   apply 
-    (makeNullContinuation useEnv)
+    continuation
     transformer
-    [lisp, IOFunc explicitRenamingRename, IOFunc explicitRenamingCompare] 
- where
- -- From clinger's paper "Hygienic Macros Through Explicit Renaming":
- --
- -- The expression returned by the transformation procedure
- -- will be expanded in the syntactic environment obtained
- -- from the syntactic environment of the macro application
- -- by binding any fresh identifiers in the syntactic
- -- environment in which the macro was defined. This means
- -- that a renamed identifier will denote the same thing as
- -- the original identifier unless the transformation
- -- procedure that renamed the identifier placed an
- -- occurrence of it in a binding position.
- --
- -- The renaming procedure acts as a mathematical function
- -- in the sense that the idenfiers obtained from any two
- -- calls with the same argument will be the same in
- -- the sense of eqv?. It is an error if the renaming
- -- procedure is called after the transformation
- -- procedure has returned.
- explicitRenamingRename :: [LispVal] -> IOThrowsError LispVal
- explicitRenamingRename [Atom a] = do
-   isDef <- liftIO $ isRecBound defEnv a
-   if isDef
-      then do
-        isRenamed <- liftIO $ isRecBound renameEnv a
-        if isRenamed
-           then do
-             renamed <- getVar renameEnv a
-             return renamed
-           else do
-             value <- getVar defEnv a
-             Atom renamed <- _gensym a -- Unique name
-             _ <- defineVar useEnv renamed value -- divert value to Use Env
-             _ <- defineVar renameEnv a $ Atom renamed -- Record renamed sym
-             return $ Atom renamed
-      else
-        return $ Atom a
- explicitRenamingRename form = throwError $ Default $ "Unable to rename: " ++ show form
+    [lisp, 
+     IOFunc $ explicitRenamingRename useEnv renameEnv defEnv, 
+     IOFunc $ explicitRenamingCompare useEnv renameEnv defEnv] 
 
- -- The explicit renaming compare function
- explicitRenamingCompare  :: [LispVal] -> IOThrowsError LispVal
- explicitRenamingCompare values@[a, b] = do
-   return $ Bool $ eqVal a b
- explicitRenamingCompare form = throwError $ 
-    Default $ "Unable to compare: " ++ show form
+-- |The explicit renaming "rename" function
+--
+-- From clinger's paper "Hygienic Macros Through Explicit Renaming":
+--
+-- The expression returned by the transformation procedure
+-- will be expanded in the syntactic environment obtained
+-- from the syntactic environment of the macro application
+-- by binding any fresh identifiers in the syntactic
+-- environment in which the macro was defined. This means
+-- that a renamed identifier will denote the same thing as
+-- the original identifier unless the transformation
+-- procedure that renamed the identifier placed an
+-- occurrence of it in a binding position.
+--
+-- The renaming procedure acts as a mathematical function
+-- in the sense that the idenfiers obtained from any two
+-- calls with the same argument will be the same in
+-- the sense of eqv?. It is an error if the renaming
+-- procedure is called after the transformation
+-- procedure has returned.
+explicitRenamingRename :: Env -> Env -> Env -> [LispVal] -> IOThrowsError LispVal
+explicitRenamingRename useEnv renameEnv defEnv [Atom a] = do
+  isDef <- liftIO $ isRecBound defEnv a
+  if isDef
+     then do
+       isRenamed <- liftIO $ isRecBound renameEnv a
+       if isRenamed
+          then do
+            renamed <- getVar renameEnv a
+            return renamed
+          else do
+            value <- getVar defEnv a
+            Atom renamed <- _gensym a -- Unique name
+            _ <- defineVar useEnv renamed value -- divert value to Use Env
+            _ <- defineVar renameEnv a $ Atom renamed -- Record renamed sym
+            return $ Atom renamed
+     else
+       return $ Atom a
+explicitRenamingRename _ _ _ form = throwError $ Default $ "Unable to rename: " ++ show form
+
+-- |The explicit renaming compare function
+explicitRenamingCompare :: Env -> Env -> Env -> [LispVal] -> IOThrowsError LispVal
+explicitRenamingCompare useEnv renameEnv defEnv values@[a, b] = do
+  return $ Bool $ eqVal a b
+explicitRenamingCompare _ _ _ form = throwError $ 
+   Default $ "Unable to compare: " ++ show form
 ----
 
 
