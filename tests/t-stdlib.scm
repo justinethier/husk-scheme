a219b896537b49ce4708ee8d41cbfb814e77f195
Justin Ethier <justin.ethier@gmail.com>
2012-10-19 23:12:10 -0400

    All of the existing test cases pass now!

--- tests/t-stdlib.scm
+++ tests/t-stdlib.scm
@@ -86,11 +86,8 @@
 ;;
 ; End test failures
 
-; TODO: the following two lines cause an infinite loop!
-;(set-cdr! x x)
-;(assert/equal (list? x)  #f)
-(assert/equal #t 'TODO) ; TODO: a placeholder for the above
-;
+(set-cdr! x x)
+(assert/equal (list? x)  #f)
 
 (assert/equal (pair? (list 1 2 3)) #t)
 (assert/equal (pair? 1) #f)
