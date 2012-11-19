096af54d5e79f9e65d0c86110b93f2b3c753f14e
Justin <justin@justin-desktop.(none)>
2012-11-19 17:31:40 -0500

    Experimenting with quasiquote as a macro

--- stdlib.scm
+++ stdlib.scm
@@ -64,41 +64,41 @@
 (define (sum . lst)     (foldl + 0 lst))
 (define (product . lst) (foldl * 1 lst))
 
-;; Quasi-quotation as a macro
-;; Based on code from chibi-scheme
-;(define-syntax quasiquote
-;  (er-macro-transformer
-;   (lambda (expr rename compare)
-;     (write "testing")
-;     (define (qq x d)
-;       (cond
-;        ((pair? x)
-;         (cond
-;          ((compare (rename 'unquote) (car x))
-;           (if (<= d 0)
-;               (cadr x)
-;               (list (rename 'list) (list (rename 'quote) 'unquote)
-;                     (qq (cadr x) (- d 1)))))
-;          ((compare (rename 'unquote-splicing) (car x))
-;           (if (<= d 0)
-;               (list (rename 'cons) (qq (car x) d) (qq (cdr x) d))
-;               (list (rename 'list) (list (rename 'quote) 'unquote-splicing)
-;                     (qq (cadr x) (- d 1)))))
-;          ((compare (rename 'quasiquote) (car x))
-;           (list (rename 'list) (list (rename 'quote) 'quasiquote)
-;                 (qq (cadr x) (+ d 1))))
-;          ((and (<= d 0) (pair? (car x))
-;                (compare (rename 'unquote-splicing) (caar x)))
-;           (if (null? (cdr x))
-;               (cadr (car x))
-;               (list (rename 'append) (cadr (car x)) (qq (cdr x) d))))
-;          (else
-;           (list (rename 'cons) (qq (car x) d) (qq (cdr x) d)))))
-;        ((vector? x) (list (rename 'list->vector) (qq (vector->list x) d)))
-;        ((if (symbol? x) #t (null? x)) (list (rename 'quote) x))
-;        (else x)))
-;     (qq (cadr expr) 0))))
-;;     ((qq (cadr expr) 0)))))
+; Quasi-quotation as a macro
+; Based on code from chibi-scheme
+(define-syntax quasiquote
+  (er-macro-transformer
+   (lambda (expr rename compare)
+     (write "testing")
+     (define (qq x d)
+       (cond
+        ((pair? x)
+         (cond
+          ((compare (rename 'unquote) (car x))
+           (if (<= d 0)
+               (cadr x)
+               (list (rename 'list) (list (rename 'quote) 'unquote)
+                     (qq (cadr x) (- d 1)))))
+          ((compare (rename 'unquote-splicing) (car x))
+           (if (<= d 0)
+               (list (rename 'cons) (qq (car x) d) (qq (cdr x) d))
+               (list (rename 'list) (list (rename 'quote) 'unquote-splicing)
+                     (qq (cadr x) (- d 1)))))
+          ((compare (rename 'quasiquote) (car x))
+           (list (rename 'list) (list (rename 'quote) 'quasiquote)
+                 (qq (cadr x) (+ d 1))))
+          ((and (<= d 0) (pair? (car x))
+                (compare (rename 'unquote-splicing) (caar x)))
+           (if (null? (cdr x))
+               (cadr (car x))
+               (list (rename 'append) (cadr (car x)) (qq (cdr x) d))))
+          (else
+           (list (rename 'cons) (qq (car x) d) (qq (cdr x) d)))))
+        ((vector? x) (list (rename 'list->vector) (qq (vector->list x) d)))
+        ((if (symbol? x) #t (null? x)) (list (rename 'quote) x))
+        (else x)))
+     (qq (cadr expr) 0))))
+;     ((qq (cadr expr) 0)))))
 
 ; Forms from R5RS for and/or
 (define-syntax and
