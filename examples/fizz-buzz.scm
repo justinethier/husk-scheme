90880bdb1dc4d7ee02423b0f1095bfd015fb54f1
Justin Ethier <justin.ethier@gmail.com>
2012-11-30 23:34:09 -0500

    Initial file

---
+++ examples/fizz-buzz.scm
@@ -0,0 +1,11 @@
+; A solution to FizzBuzz using named let:
+; <http://www.codinghorror.com/blog/2007/02/why-cant-programmers-program.html>
+(let fizz-buzz ((i 1))
+    (if (>= 100 i)
+        (begin
+            (cond
+                ((= 0 (modulo i 15)) (write 'fizzbuzz))
+                ((= 0 (modulo i 3)) (write 'fizz))
+                ((= 0 (modulo i 5)) (write 'buzz))
+                (else (write i)))
+            (fizz-buzz (+ i 1)))))
