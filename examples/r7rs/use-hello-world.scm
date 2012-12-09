7b1d6b0aa9f7ae62795803703218155128622313
Justin Ethier <justin.ethier@gmail.com>
2012-12-09 15:01:33 -0500

    Debugging module meta language file

--- examples/r7rs/use-hello-world.scm
+++ examples/r7rs/use-hello-world.scm
@@ -1,7 +1,13 @@
 ; A simple example of using an R7RS module
 ; Note: husk does not support this yet!
-(import (scheme base)
-        (scheme write)
-        (hello world))
+
+; TODO: should be able to use the three lines below
+;(import (scheme base)
+;        (scheme write)
+;        (hello world))
+; But instead, we debug using these:
+(load "../../lib/modules.scm")
+(repl-import (hello world))
+; end import
 (write hello)
 (newline)
