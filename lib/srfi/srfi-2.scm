b46136bee7160ee290a9a9de515ffbf51a03668f
Justin Ethier <justin.ethier@gmail.com>
2012-12-13 15:48:53 -0500

    Added test cases

--- lib/srfi/srfi-2.scm
+++ lib/srfi/srfi-2.scm
@@ -9,3 +9,58 @@
     ((and-let* ((expr) . rest) . body)
      (let ((tmp expr))
        (and tmp (and-let* rest . body))))))
+
+
+; Tests from the SRFI proposal:
+; linked from http://srfi.schemers.org/srfi-2/srfi-2.html
+
+; No claws
+(expect  '(and-let* () 1) 1)
+(expect  '(and-let* () 1 2) 2)
+(expect  '(and-let* () ) #t)
+
+(must-be-a-syntax-error '(and-let* #f #t) )
+(must-be-a-syntax-error '(and-let* #f) )
+
+; One claw, no body
+(expect '(let ((x #f)) (and-let* (x))) #f)
+(expect '(let ((x 1)) (and-let* (x))) 1)
+(expect '(let ((x 1)) (and-let* ( (x) ))) 1)
+(expect '(let ((x 1)) (and-let* ( ((+ x 1)) ))) 2)
+(expect '(and-let* ((x #f)) ) #f)
+(expect '(and-let* ((x 1)) ) 1)
+(must-be-a-syntax-error '(and-let* ( #f (x 1))) )
+
+; two claws, no body
+(expect '(and-let* ( (#f) (x 1)) ) #f)
+(must-be-a-syntax-error '(and-let* (2 (x 1))) )
+(expect '(and-let* ( (2) (x 1)) ) 1)
+(expect '(and-let* ( (x 1) (2)) ) 2)
+(expect '(and-let* ( (x 1) x) ) 1)
+(expect '(and-let* ( (x 1) (x)) ) 1)
+
+; two claws, body
+(expect '(let ((x #f)) (and-let* (x) x)) #f)
+(expect '(let ((x "")) (and-let* (x) x)) "")
+(expect '(let ((x "")) (and-let* (x)  )) "")
+(expect '(let ((x 1)) (and-let* (x) (+ x 1))) 2)
+(expect '(let ((x #f)) (and-let* (x) (+ x 1))) #f)
+(expect '(let ((x 1)) (and-let* (((positive? x))) (+ x 1))) 2)
+(expect '(let ((x 1)) (and-let* (((positive? x))) )) #t)
+(expect '(let ((x 0)) (and-let* (((positive? x))) (+ x 1))) #f)
+(expect '(let ((x 1)) (and-let* (((positive? x)) (x (+ x 1))) (+ x 1)))  3)
+(expect
+ '(let ((x 1)) (and-let* (((positive? x)) (x (+ x 1)) (x (+ x 1))) (+ x 1)))
+  4
+  )
+
+(expect '(let ((x 1)) (and-let* (x ((positive? x))) (+ x 1))) 2)
+(expect '(let ((x 1)) (and-let* ( ((begin x)) ((positive? x))) (+ x 1))) 2)
+(expect '(let ((x 0)) (and-let* (x ((positive? x))) (+ x 1))) #f)
+(expect '(let ((x #f)) (and-let* (x ((positive? x))) (+ x 1))) #f)
+(expect '(let ((x #f)) (and-let* ( ((begin x)) ((positive? x))) (+ x 1))) #f)
+
+(expect  '(let ((x 1)) (and-let* (x (y (- x 1)) ((positive? y))) (/ x y))) #f)
+(expect  '(let ((x 0)) (and-let* (x (y (- x 1)) ((positive? y))) (/ x y))) #f)
+(expect  '(let ((x #f)) (and-let* (x (y (- x 1)) ((positive? y))) (/ x y))) #f)
+(expect  '(let ((x 3)) (and-let* (x (y (- x 1)) ((positive? y))) (/ x y))) (/ 3 2))
