f9120b13827612dbc344d2e9a054db90f6668a6b
Justin Ethier <justin.ethier@gmail.com>
2012-12-13 14:17:55 -0500

    Added a stub, still need to test

---
+++ lib/srfi/srfi-2.scm
@@ -0,0 +1,11 @@
+; TODO: this is untested!
+(define-syntax and-let*
+  (syntax-rules ()
+    ((and-let* () body ...)
+     (begin body ...))
+    ((and-let* ((var expr) . rest) . body)
+     (let ((var expr))
+       (and var (and-let* rest . body))))
+    ((and-let* ((expr) . rest) . body)
+     (let ((tmp expr))
+       (and tmp (and-let* rest . body))))))
