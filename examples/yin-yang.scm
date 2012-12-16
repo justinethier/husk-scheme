98b52111315a92b540048761e3c9defb200ea569
Justin Ethier <justin.ethier@gmail.com>
2012-12-15 21:44:46 -0500

    Added a header comment block

--- examples/yin-yang.scm
+++ examples/yin-yang.scm
@@ -1,3 +1,10 @@
+;;; 
+;;; Justin Ethier
+;;; husk-scheme
+;;;
+;;; Example program for First class continuations, from
+;;; http://en.wikipedia.org/wiki/Scheme_(programming_language)#First-class_continuations
+;;;
 (let* ((yin
         ((lambda (cc) (display "@") cc) (call-with-current-continuation (lambda (c) c))))
        (yang
