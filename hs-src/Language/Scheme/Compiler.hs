681b2afa6e37c036f8cc0d1f958b316bffd2b93a
Justin Ethier <justin.ethier@gmail.com>
2012-12-24 10:42:54 -0500

    Added test code for (load)

--- hs-src/Language/Scheme/Compiler.hs
+++ hs-src/Language/Scheme/Compiler.hs
@@ -650,16 +650,14 @@ compile env (List [Atom "hash-table-delete!", Atom var, rkey]) copts = do
 -- TODO: eval env cont fargs@(List (Atom "hash-table-delete!" : args)) = do
 
 -- TODO:
--- Testing this out; idea is to compile-in any code injected via
--- (load)...
---
--- compile env (List [Atom "load", String filename]) copts = do -- TODO: allow filename from a var, support env optional arg
---  Atom symEntryPt <- _gensym "load"
---  result <- compileLisp env filename symEntryPt $ coptsNextFunc copts
---  return $ result ++ 
---    [createAstFunc copts [
---      AstValue $ "  result <- " ++ symEntryPt ++ " env cont (Nil \"\") Nothing",
---      createAstCont copts "result" ""]]
+-- Testing this out; idea is to compile-in any code injected via load
+compile env (List [Atom "load", String filename]) copts = do -- TODO: allow filename from a var, support env optional arg
+ Atom symEntryPt <- _gensym "load"
+ result <- compileLisp env filename symEntryPt Nothing
+ return $ result ++ 
+   [createAstFunc copts [
+    AstValue $ "  result <- " ++ symEntryPt ++ " env (makeNullContinuation env) (Nil \"\") Nothing",
+    createAstCont copts "result" ""]]
 
 -- FUTURE: eventually it should be possible to evaluate the args instead of assuming
 -- that they are all strings, but lets keep it simple for now
