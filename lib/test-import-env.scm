bac3b7ab12c60428e9946e6d20388244a90a15ce
Justin Ethier <justin.ethier@gmail.com>
2013-01-31 15:14:39 -0500

    Removed old file

--- lib/test-import-env.scm
+++
@@ -1,7 +0,0 @@
-; Simple test to prove if %import into env is working
-; A temporary program; remove once this is working
-(define env (make-environment))
-(write (list 'initial: (print-env env)))
-(%import env (current-environment) #f #f)
-(write (list 'final: (print-env env)))
-(write (eval '(car (list 1 2 3)) env))
