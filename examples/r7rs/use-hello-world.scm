d2dc52e249e917fd731fb602d509b62f1640bccc
Justin Ethier <justin.ethier@gmail.com>
2013-02-11 21:58:49 -0500

    Removed debug code

--- examples/r7rs/use-hello-world.scm
+++ examples/r7rs/use-hello-world.scm
@@ -1,13 +1,8 @@
 ; A simple example of using an R7RS module
-; Note: husk does not support this yet!
 
-; TODO: should be able to use the three lines below
-;(import (scheme base)
+(import (scheme base)
 ;        (scheme write)
-;        (hello world))
-; But instead, we debug using these:
-(load "../../lib/modules.scm")
-(repl-import (hello world))
-; end import
+        (hello world))
+
 (write hello)
 (newline)
