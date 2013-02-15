86eaec5299870378514aec5ef3e6e05ebcf19284
Justin Ethier <justin.ethier@gmail.com>
2013-02-15 14:39:12 -0500

    Cleaned up

--- examples/clear-vt100.scm
+++ examples/clear-vt100.scm
@@ -1,8 +1,9 @@
+; A simple example to clear a VT100 terminal
 (display
-(string 
-    (integer->char #x1B) 
-    #\[
-    #\H
-    (integer->char #x1B)
-    #\[
-    #\J))
+    (string 
+        (integer->char #x1B) 
+        #\[
+        #\H
+        (integer->char #x1B)
+        #\[
+        #\J))
