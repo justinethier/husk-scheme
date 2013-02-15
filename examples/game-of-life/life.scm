68090ae0506cebbe667411918409f5f87444d6b0
Justin Ethier <justin.ethier@gmail.com>
2013-02-15 14:39:18 -0500

    Initial file

---
+++ examples/game-of-life/life.scm
@@ -0,0 +1,15 @@
+;; Main program.
+(import (scheme base)
+        (only (example life) life)
+        (rename (prefix (example grid) grid-)
+                (grid-make make-grid)))
+
+;; Initialize a grid with a glider.
+(define grid (make-grid 24 24))
+(grid-set! grid 1 1 #t)
+(grid-set! grid 2 2 #t)
+(grid-set! grid 3 0 #t)
+(grid-set! grid 3 1 #t)
+(grid-set! grid 3 2 #t)
+;; Run for 80 iterations.
+(life grid 80)
