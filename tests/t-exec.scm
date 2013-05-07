34c2f65d1ab0d58b2f5f51d3fe2f6ccd16bee716
Justin Ethier <justin.ethier@gmail.com>
2013-05-07 14:31:45 -0400

    Labelled each test case

--- tests/t-exec.scm
+++ tests/t-exec.scm
@@ -8,11 +8,13 @@
 ;;
 (unit-test-start "control features")
 
-(assert/equal (procedure? car) #t)
-(assert/equal (procedure? 'car) #f)
-(assert/equal (procedure? (lambda (x) (* x x))) #t)
-(assert/equal (procedure? '(lambda (x) (* x x)))  #f)
-(assert/equal (call-with-current-continuation procedure?) #t)
+(assert/equal (procedure? car) #t "car is a procedure")
+(assert/equal (procedure? 'car) #f "symbol is not a proc")
+(assert/equal (procedure? (lambda (x) (* x x))) #t "lambda is a proc")
+(assert/equal (procedure? '(lambda (x) (* x x)))  #f "lambda sym not a proc")
+(assert/equal (call-with-current-continuation procedure?) 
+              #t
+              "call/cc is a proc")
 
 (assert/equal
   (let ((v (make-vector 5)))
@@ -20,15 +22,16 @@
                 (vector-set! v i (* i i)))
               '(0 1 2 3 4))
      v)
-  '#(0 1 4 9 16))
+  '#(0 1 4 9 16)
+  "vector results")
 
-(assert/equal (apply + (list 3 4)) 7) 
+(assert/equal (apply + (list 3 4)) 7 "apply results") 
 
 (define compose
     (lambda (f g)
           (lambda args
                   (f (apply g args)))))
-(assert/equal ((compose sqrt *) 12 75) 30.0)
+(assert/equal ((compose sqrt *) 12 75) 30.0 "compose")
 
 (assert/equal
  (let ((path '())
@@ -46,7 +49,8 @@
     (if (< (length path) 4)
         (c 'talk2)
         (reverse path))))
- '(connect talk1 disconnect connect talk2 disconnect))
+ '(connect talk1 disconnect connect talk2 disconnect)
+ "chat")
 
 (assert/equal
   (let ((path '())
@@ -64,7 +68,8 @@
     (if (< (length path) 4)
         (c 'talk2)
         (reverse path))))
- '(connect talk1 disconnect talk2)) ; Continuation not invoked during dynamic extent, so before/after are not invoked
+ '(connect talk1 disconnect talk2)
+ "dynamic-wind not invoked") ; Continuation not invoked during dynamic extent, so before/after are not invoked
 
 (assert/equal
   (let ((path '())
@@ -82,7 +87,8 @@
     (if (< (length path) 4)
         (c 'talk2)
         (reverse path))))
- '(connect talk1 disconnect talk2 talk1 disconnect)) ; Because continuation captured during before, thunk/after are called twice
+ '(connect talk1 disconnect talk2 talk1 disconnect)
+ "dynamic wind after called twice" ) ; Because continuation captured during before, thunk/after are called twice
 
 (define data '())
 (dynamic-wind
@@ -104,7 +110,9 @@
         (reverse path)))))))
   (lambda () (set! data (append data "Invoked outer (after)"))))
 
-(assert/equal  data 
-              '("Invoked outer (before)" connect talk1 disconnect connect talk2 disconnect . "Invoked outer (after)"))
+(assert/equal 
+    data 
+    '("Invoked outer (before)" connect talk1 disconnect connect talk2 disconnect . "Invoked outer (after)")
+    "dynamic-wind before and after")
 
 (unit-test-handler-results)
