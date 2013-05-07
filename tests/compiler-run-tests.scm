147b06586f57c42a5e02f0325dbbfd6474ec3bea
Justin Ethier <justin.ethier@gmail.com>
2013-05-07 15:08:14 -0400

    Added more test cases
    Added back all tests that run to completion, and noted the ones that do not.

--- tests/compiler-run-tests.scm
+++ tests/compiler-run-tests.scm
@@ -12,29 +12,29 @@
 (let () (load "t-delay.scm"))
 (let () (load "t-eval.scm"))
 (let () (load "t-exec.scm"))
-;(let () (load "t-extensions.scm"))
-;(let () (load "t-hashtable.scm"))
-;(let () (load "t-io.scm"))
-;(let () (load "t-iteration.scm"))
-;(let () (load "t-numerical-ops.scm"))
-;(let () (load "t-parser.scm"))
-;(let () (load "t-scoping.scm"))
-;(let () (load "t-special-forms.scm"))
-;(let () (load "t-standard-procedures.scm"))
+(let () (load "t-extensions.scm"))
+(let () (load "t-hashtable.scm"))
+(let () (load "t-io.scm"))
+(let () (load "t-iteration.scm"))
+(let () (load "t-numerical-ops.scm"))
+(let () (load "t-parser.scm"))
+;(let () (load "t-scoping.scm")) ; "Not implemented" error
+(let () (load "t-special-forms.scm"))
+(let () (load "t-standard-procedures.scm"))
 ;(let () (load "t-stdlib.scm"))
-;(let () (load "t-storage.scm"))
-;(let () (load "t-string.scm"))
-;(let () (load "t-vector.scm"))
-;(let () (load "t-bytevector.scm"))
+(let () (load "t-storage.scm"))
+(let () (load "t-string.scm"))
+;(let () (load "t-vector.scm")) ; Unbound variable error, looks macro related
+(let () (load "t-bytevector.scm"))
 ;; Macro tests
-;(let () (load "t-macro.scm"))
-;(let () (load "t-macro-lists.scm"))
-;(let () (load "t-macro-hygiene.scm"))
-;(let () (load "t-macro-ref-trans.scm"))
-;(let () (load "t-er-macro.scm"))
+(let () (load "t-macro.scm"))
+(let () (load "t-macro-lists.scm"))
+;(let () (load "t-macro-hygiene.scm")) ; Unbound variable error
+;(let () (load "t-macro-ref-trans.scm")) ; Unbound variable error
+(let () (load "t-er-macro.scm"))
 ;; SRFI tests
-;(let () (load "t-srfi-1.scm"))
-;(let () (load "t-srfi-2.scm"))
+;(let () (load "t-srfi-1.scm")) ; There is a *lot* of code here, may be difficult to test or have long compiles
+;(let () (load "t-srfi-2.scm")) ; Get a and-let* not defined error
 ;; Summarize test results
 (let () (load "summarize.scm"))
 (delete-file "scm-unit.tmp")
