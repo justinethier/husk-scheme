87dda117f83c88cbb35e658a326851b1868bb0ec
Justin Ethier <justin.ethier@gmail.com>
2013-05-10 22:42:46 -0400

    Added labels for each test case

--- tests/t-standard-procedures.scm
+++ tests/t-standard-procedures.scm
@@ -19,19 +19,24 @@
 
 (assert/equal
     (eqv? 'a 'a)
-    #t)
+    #t
+    "a symbols eqv")
 (assert/equal
     (eqv? 'a 'b)
-    #f)
+    #f
+    "a/b symbols not eqv")
 (assert/equal
     (eqv? 2 2)
-    #t)
+    #t
+    "numbers eqv")
 (assert/equal
     (eqv? '() '())
-    #t)
+    #t
+    "nil lists eqv")
 (assert/equal
     (eqv? 100000000 100000000)
-    #t)
+    #t
+    "large numbers eqv")
 ; Fails R5RS test case
 ;(assert/equal
 ;    (eqv? (cons 1 2) (cons 1 2))
@@ -39,16 +44,19 @@
 (assert/equal
     (eqv? (lambda () 1)
           (lambda () 2))
-    #f)
+    #f
+    "functions not eqv")
 (assert/equal
     (eqv? #f 'nil)
-    #f)
+    #f
+    "bool / nil not eqv")
 
 ; R5RS test case
 (assert/equal
     (let ((p (lambda (x) x)))
          (eqv? p p))
-    #t)
+    #t
+    "p variables eqv")
 
 (define gen-counter
     (lambda ()
@@ -59,7 +67,8 @@
 (assert/equal
     (let ((g (gen-counter)))
         (eqv? g g))
-    #t)
+    #t
+    "g gen counter eqv")
 
 (define gen-loser
     (lambda ()
@@ -69,7 +78,8 @@
 (assert/equal
     (let ((g (gen-loser)))
         (eqv? g g))
-    #t)
+    #t
+    "g gen loser eqv")
 
 ;(eqv? (gen-loser) (gen-loser))
 ;                                ===>  unspecified
@@ -83,55 +93,58 @@
     (letrec ((f (lambda () (if (eqv? f g) 'f 'both)))
                   (g (lambda () (if (eqv? f g) 'g 'both))))
         (eqv? f g))
-    #f)
+    #f
+    "f / g not eqv")
 
 (assert/equal
     (let ((x '(a)))
         (eqv? x x))
-    #t)
+    #t
+    "x eqv")
 
 ; Many tests that do not meet spec
-(assert/equal (eq? 'a 'a) #t)
-(assert/equal (eq? '(a) '(a)) #t)
+(assert/equal (eq? 'a 'a) #t "a's eq")
+(assert/equal (eq? '(a) '(a)) #t "a lists eq")
 ;(assert/equal (eq? (list 'a) (list 'a))  #f)
-(assert/equal (eq? "a" "a") #t)
-(assert/equal (eq? "" "") #t)
+(assert/equal (eq? "a" "a") #t "a strings eq")
+(assert/equal (eq? "" "") #t "empty strings eq")
 (assert/equal (eq? '() '())              #t)
-(assert/equal (eq? 2 2) #t)
-(assert/equal (eq? #\A #\A) #t)
+(assert/equal (eq? 2 2) #t "2's eq")
+(assert/equal (eq? #\A #\A) #t "A chars eq")
 ;(assert/equal (eq? car car)              #t)
 (assert/equal (let ((n (+ 2 3)))
                    (eq? n n))
-              #t) ;  ===>  unspecified
+              #t
+              "n sum eq") ;  ===>  unspecified
 (assert/equal (let ((x '(a)))
-    (eq? x x))              #t)
+    (eq? x x))              #t "x as a list eq")
 (assert/equal (let ((x '#()))
-    (eq? x x))              #t)
+    (eq? x x))              #t "x as vector eq")
 ;(assert/equal (let ((p (lambda (x) x)))
 ;    (eq? p p))              #t)
 
-(assert/equal (equal? 'a 'a) #t)
-(assert/equal (equal? '(a) '(a))         #t)
+(assert/equal (equal? 'a 'a) #t "a's equal")
+(assert/equal (equal? '(a) '(a))         #t "a lists equal")
 (assert/equal (equal? '(a (b) c)
-                '(a (b) c))              #t)
-(assert/equal (equal? "abc" "abc")       #t)
-(assert/equal (equal? 2 2)               #t)
+                '(a (b) c))              #t "bigger list equal")
+(assert/equal (equal? "abc" "abc")       #t "abc string equal")
+(assert/equal (equal? 2 2)               #t "2 equal")
 (assert/equal (equal? (make-vector 5 'a)
-                (make-vector 5 'a))      #t)
+                (make-vector 5 'a))      #t "vectors equal")
 (assert/equal (equal? (lambda (x) x)
                       (lambda (y) y))
-               #f) ; ===>  unspecified
+               #f "functions not equal") ; ===>  unspecified
 
-(assert/equal (not #t)         #f)
-(assert/equal (not 3)          #f)
-(assert/equal (not (list 3))   #f)
-(assert/equal (not #f)         #t)
-(assert/equal (not '())        #f)
-(assert/equal (not (list))     #f)
-(assert/equal (not 'nil)       #f)
+(assert/equal (not #t)         #f "not true")
+(assert/equal (not 3)          #f "not 3")
+(assert/equal (not (list 3))   #f "not list 3")
+(assert/equal (not #f)         #t "not false")
+(assert/equal (not '())        #f "not nil")
+(assert/equal (not (list))     #f "not list")
+(assert/equal (not 'nil)       #f "not nil symbol")
 
-(assert/equal (boolean? #f)     #t)
-(assert/equal (boolean? 0)      #f)
-(assert/equal (boolean? '())    #f)
+(assert/equal (boolean? #f)     #t "false is a boolean")
+(assert/equal (boolean? 0)      #f "0 is not a boolean")
+(assert/equal (boolean? '())    #f "nil is not a boolean")
 
 (unit-test-handler-results)
