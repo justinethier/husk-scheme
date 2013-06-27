9926b7cc4df8228c7e85666e96cc2b3aab310c56
Justin Ethier <justin.ethier@gmail.com>
2013-06-26 21:46:23 -0400

    Added indicators for program/library

--- tests/libraries/simplest/libs/list.sld
+++ tests/libraries/simplest/libs/list.sld
@@ -8,5 +8,6 @@
     (export list2)
     (begin
         (define (list2 . objs)  objs)
+        (define x 'libs-list)
         (define not-exported 'should-not-be-exported)
         ))
--- tests/libraries/simplest/test-list.scm
+++ tests/libraries/simplest/test-list.scm
@@ -1,8 +1,12 @@
 ;; Compile with 
 ;; huskc --nolibs test-list.scm
 
+(define x 'main-program)
+
 (import (libs list))
 
 (write  
     (list2 1 2 3))
-not-exported ; Should throw an error due to env restrictions
+;not-exported ; Should throw an error due to env restrictions
+
+(write x)
