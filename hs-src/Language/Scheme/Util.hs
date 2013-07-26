21fd3d21a6b1bbfe54021fed68359bab6fe510af
Justin Ethier <justin.ethier@gmail.com>
2013-07-25 22:55:12 -0400

    Migrated strip function from shell.hs

--- hs-src/Language/Scheme/Util.hs
+++ hs-src/Language/Scheme/Util.hs
@@ -11,12 +11,9 @@ This module contains general-purpose utility functions
 -}
 
 module Language.Scheme.Util
-    (
-      escapeBackslashes
+    ( escapeBackslashes
+    , strip
     ) where
--- import qualified Paths_husk_scheme as PHS (getDataFileName)
--- import Language.Scheme.Types
--- import Language.Scheme.Variables
 
 -- |A utility function to escape backslashes in the given string
 escapeBackslashes :: String -> String
@@ -24,3 +21,10 @@ escapeBackslashes s = foldr step [] s
   where step x xs  | x == '\\'  = '\\' : '\\' : xs
                    | otherwise =  x : xs 
 
+-- | Remove leading/trailing white space from a string; based on corresponding 
+--   Python function. Code taken from: 
+--
+--   http://gimbo.org.uk/blog/2007/04/20/splitting-a-string-in-haskell/
+strip :: String -> String
+strip s = dropWhile ws $ reverse $ dropWhile ws $ reverse s
+    where ws = (`elem` [' ', '\n', '\t', '\r'])
