e736e3f107d95e84b9e3b8908f498eaa23d016aa
Justin Ethier <justin.ethier@gmail.com>
2013-08-02 23:09:07 -0400

    Stubbed out running scripts

--- scripts/build.scm
+++ scripts/build.scm
@@ -1,14 +1,28 @@
-TODO: list of shell commands to run
-("cmd 1" "cmd 2" ...)
-map over them to execute script (or better, chain over in case one fails)
+; TODO: list of shell commands to run
+; ("cmd 1" "cmd 2" ...)
+; map over them to execute script (or better, chain over in case one fails)
+; 
+; steps:
+; - ask user about change log, blog post? (is there a better way to integrate these?)
+; - run tests
+; - prompt user to review tests
+; - if OK, build docs
+; - if OK, then prompt for version number and tag it
+; - copy docs to gh-pages, commit, push
+; - checkout master branch again (all releases off of master)
+; - make sdist
+; - prompt to upload to hackage, freecode, etc
 
-steps:
-- ask user about change log, blog post? (is there a better way to integrate these?)
-- run tests
-- prompt user to review tests
-- if OK, build docs
-- if OK, then prompt for version number and tag it
-- copy docs to gh-pages, commit, push
-- checkout master branch again (all releases off of master)
-- make sdist
-- prompt to upload to hackage, freecode, etc
+(define-syntax script
+  (syntax-rules ()
+    ((_ cmd) (system cmd))
+    ((_ cmd cmds ...)
+     (if (= (system cmd) 0)
+         (chain cmds ...)))))
+
+(script
+;    "git checkout master"
+    "make test && make doc"
+;;    "git checkout gh-pages"
+    "echo \"TODO\"")
+
