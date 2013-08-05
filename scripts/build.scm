2667262b0574988b640ba7ee741ccd86a20505bb
Justin Ethier <justin.ethier@gmail.com>
2013-08-05 14:46:40 -0400

    WIP

--- scripts/build.scm
+++ scripts/build.scm
@@ -16,9 +16,11 @@
     "make test"
 ; TODO:    "make testc"
     "echo \"Make sure no tests failed, and then press Enter to continue\" ; read temp"
+    "make sdist"
 
 ; TODO: ?????  - if OK, then prompt for version number and tag it
 ; TODO: (string-append "git tag -a v" *build-number* " -m \"Version " *build-number* "\"")
+; TODO: push tag to origin
 
     "make doc"
     "git checkout gh-pages"
@@ -27,12 +29,12 @@
     ;    (begin (write "updating docs")
     ;           (string-append "mkdir API/" *build-number* " ; cp dist/doc/html/husk-scheme/* API/" *build-number*)))
     (string-append "mkdir API/" *build-number* " ; cp dist/doc/html/husk-scheme/* API/" *build-number*)
+; TODO: _includes/sidebar*.html still need to be updated to reference a new build, need to address that somehow
     "echo \"API documentation updated; press Enter to commit and push to github\" ; read temp"
     (string-append "cd API/" *build-number* " && git add * && git commit * -m \"Build script added/updated API documentation for version " *build-number* "\"")
     "git push origin gh-pages"
 ; - copy docs to gh-pages, commit, push
 ; - checkout master branch again (all releases off of master)
     "git checkout repl-dev" ; TESTING, should be master
-    "make sdist"
     "echo \"\""
     "echo \"TODO: upload to hackage, freecode, etc\"")
