8c0adfaa5d78c7a163dd4688d80c4a1277cb3159
Justin Ethier <justin.ethier@gmail.com>
2013-08-05 22:12:57 -0400

    Cleaned up build script

--- scripts/build.scm
+++ scripts/build.scm
@@ -2,15 +2,23 @@
 
 (define-syntax script
   (syntax-rules ()
-;    ((_) 'done)
     ((_ cmd) (system cmd))
     ((_ cmd cmds ...)
      (if (= (system cmd) 0)
          (script cmds ...)))))
 
 (script
+    "echo \"husk scheme build system\""
+    "echo \"Before continuing, make sure:\""
+    "echo \"\""
+    "echo \"(1) Change log is updated\""
+    "echo \"(2) Build number has been updated in this script, cabal file, and Core.hs\""
+    "echo \"(3) Release notes have been added to gh-pages\""
+    "echo \"(4) This is the master branch. All releases must be off of master!!\""
+    "echo \"\""
+    "echo \"Press Enter to continue\" ; read temp"
+    
     (string-append "Building " *build-number* " ...")
-; TODO: - ask user about change log, blog post? (is there a better way to integrate these?)
     "make clean"
     "make"
     "make test"
@@ -18,25 +26,26 @@
     "echo \"Make sure no tests failed, and then press Enter to continue\" ; read temp"
     "make sdist"
 
-; TODO: ?????  - if OK, then prompt for version number and tag it
-; TODO: (string-append "git tag -a v" *build-number* " -m \"Version " *build-number* "\"")
-; TODO: push tag to origin
-
     "make doc"
     "git checkout gh-pages"
-    ;(if (file-exists? (string-append "API/" *build-number* "/index.html"))
-    ;    "echo \"docs up-to-date\", skipping"
-    ;    (begin (write "updating docs")
-    ;           (string-append "mkdir API/" *build-number* " ; cp dist/doc/html/husk-scheme/* API/" *build-number*)))
+        ;(if (file-exists? (string-append "API/" *build-number* "/index.html"))
+        ;    "echo \"docs up-to-date\", skipping"
+        ;    (begin (write "updating docs")
+        ;           (string-append "mkdir API/" *build-number* " ; cp dist/doc/html/husk-scheme/* API/" *build-number*)))
     (string-append "mkdir API/" *build-number* " ; cp dist/doc/html/husk-scheme/* API/" *build-number*)
-; TODO: _includes/sidebar*.html still need to be updated to reference a new build, need to address that somehow
-; Use the command: sed -i '17s/.*/husk_build_number:  3.12/' _config.yml
-; and save changes using git commit
+    ; Update build number to point to latest API docs, maybe other stuff
+    (string-append "sed -i '17s/.*/husk_build_number:  " *build-number* "/' _config.yml")
+
     "echo \"API documentation updated; press Enter to commit and push to github\" ; read temp"
     (string-append "cd API/" *build-number* " && git add * && git commit * -m \"Build script added/updated API documentation for version " *build-number* "\"")
+    "git commit _config.yml -m \"Build script updated build number\""
     "git push origin gh-pages"
-; - copy docs to gh-pages, commit, push
-; - checkout master branch again (all releases off of master)
-    "git checkout repl-dev" ; TESTING, should be master
+
+    "git checkout master"
+    "echo \"Docs done, press Enter to tag this release\" ; read temp"
+    (string-append "git tag -a v" *build-number* " -m \"Version " *build-number* "\"")
+    (string-append "git push origin v" *build-number*)
+
     "echo \"\""
-    "echo \"TODO: upload to hackage, freecode, etc\"")
+    "echo \"Tag pushed to origin, build is ready for upload to hackage, freecode, etc\"")
+
