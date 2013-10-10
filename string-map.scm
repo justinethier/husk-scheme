8f7c438f2059f7d6d4a3760c9ce3db2852375d94
Justin Ethier <justin.ethier@gmail.com>
2013-10-09 22:48:38 -0400

    Added string-for-each

--- string-map.scm
+++ string-map.scm
@@ -3,6 +3,10 @@
         (list->string 
             (apply map 
                    (cons fnc ls)))))
+(define (string-for-each fnc . sargs)
+    (let ((ls (map (lambda (v) (string->list v)) sargs)))
+        (apply for-each 
+               (cons fnc ls))))
 (write
 (string-map
     (lambda (c)
@@ -10,3 +14,12 @@
         "HAL")
 )
 ;"IBM"
+
+(write
+    (let ((v '()))
+        (string-for-each
+            (lambda (c) (set! v (cons (char->integer c) v)))
+            "abcde")
+        v)
+)
+;(101 100 99 98 97)
