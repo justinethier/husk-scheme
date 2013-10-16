49d7ec0bbb5e719d578132b2f04172d08a80e68d
Justin Ethier <justin.ethier@gmail.com>
2013-10-15 22:44:25 -0400

    Initial file

---
+++ tests/run-tests7.scm
@@ -0,0 +1,15 @@
+(load "skim-unit.scm")
+(newline)
+(display "------------------------------------------")
+(newline)
+(display "Running R7RS test suite")
+(newline)
+(newline)
+(define p (open-output-file "scm-unit.tmp"))
+(display "0\n0\n" p)
+(close-output-port p)
+; r7rs libraries
+(let () (load "t-libs.scm"))
+; Summarize test results
+(let () (load "summarize.scm"))
+(delete-file "scm-unit.tmp")
