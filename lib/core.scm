51fd1282af22201e68e49c7bd58625d489195489
Justin Ethier <justin.ethier@gmail.com>
2013-12-12 17:57:10 -0500

    Added TODO

--- lib/core.scm
+++ lib/core.scm
@@ -703,6 +703,52 @@
                  (let*-values (binding1 ...)
                     body0 body1 ...)))))
 
+;; TODO:
+;; (define-syntax define-values
+;;   (syntax-rules ()
+;;   ((define-values () expr)
+;;   (define dummy
+;;   (call-with-values (lambda () expr)
+;;   (lambda args #f))))
+;; 
+;;   ((define-values (var) expr)
+;;   (define var expr))
+;; 
+;;   ((define-values (var0 var1 ... varn) expr)
+;;   ;(begin
+;;   ((lambda ()
+;;   (define var0
+;;   (call-with-values (lambda () expr)
+;;   list))
+;;   (define var1
+;;   (let ((v (cadr var0)))
+;;   (set-cdr! var0 (cddr var0))
+;;   v)) ...
+;;   (define varn
+;;   (let ((v (cadr var0)))
+;;   (set! var0 (car var0))
+;;   v)))))
+;; 
+;; ;  ((define-values (var0 var1 ... . varn) expr)
+;; ;  ;(begin
+;; ;  ((lambda ()
+;; ;  (define var0
+;; ;  (call-with-values (lambda () expr)
+;; ;  list))
+;; ;  (define var1
+;; ;  (let ((v (cadr var0)))
+;; ;  (set-cdr! var0 (cddr var0))
+;; ;  v)) ...
+;; ;  (define varn
+;; ;  (let ((v (cdr var0)))
+;; ;  (set! var0 (car var0))
+;; ;  v)))))
+;; 
+;;   ((define-values var expr)
+;;    (define var
+;;     (call-with-values (lambda () expr)
+;;       list)))))
+
 ;;
 ;; SRFI-0 (cond-expand) from r7rs
 ;;
