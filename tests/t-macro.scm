05b62fc82ec5075b8726ff1386b158385fcdf7e1
Justin Ethier <justin.ethier@gmail.com>
2014-03-05 17:47:24 -0500

    Added test case for Issue #163

--- tests/t-macro.scm
+++ tests/t-macro.scm
@@ -474,4 +474,35 @@
             (list a b x y))) 
    '(x y x y))
 
+; Issue #163
+(define-syntax if-identifier
+  (syntax-rules ()
+    ((_ condition seq alt)
+     (let-syntax ((foo (syntax-rules () ((_) seq))))
+       (let-syntax ((test (syntax-rules ()
+                            ((_ condition) (foo))
+                            ((_ x) alt))))
+         (test foo))))))
+
+(define-syntax if-member
+  (syntax-rules ()
+    ((_ p (literals ...) seq alt)
+     (let-syntax ((foo (syntax-rules () ((_) seq))))
+       (let-syntax ((baz (syntax-rules (literals ...)
+                         ((_ literals) (foo)) ...
+                         ((_ _) alt))))
+       (baz p))))))
+
+(define-syntax foo
+  (syntax-rules ()
+    ((_ p)
+     (if-identifier p
+       (if-member p (A B C)
+         "A or B or C"
+         "Another identifier")
+       "Not identifier"))))
+(assert/equal (foo A) "A or B or C")
+; END 163
+
+
 (unit-test-handler-results)
