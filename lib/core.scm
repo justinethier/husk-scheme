a79d70611d0a357db9831bcfafd735eacfa909ba
Justin Ethier <justin.ethier@gmail.com>
2014-03-25 13:53:44 -0400

    Added parameter stubs from r7rs

--- lib/core.scm
+++ lib/core.scm
@@ -785,3 +785,47 @@
              ((check (caar ls)) `(,(rename 'begin) ,@(cdar ls)))
              (else (expand (cdr ls))))))))
 ;; END
+
+;; TODO: stubs from R7RS:
+;;(define (make-parameter init . o)
+;;  (let* ((converter
+;;           (if (pair? o) (car o) (lambda (x) x)))
+;;         (value (converter init)))
+;;    (lambda args
+;;      (cond
+;;        ((null? args)
+;;         value)
+;;        ((eq? (car args) <param-set!>)
+;;         (set! value (cadr args)))
+;;        ((eq? (car args) <param-convert>)
+;;         converter)
+;;       (else
+;;         (error "bad parameter syntax"))))))
+;;
+;;(define-syntax parameterize
+;;  (syntax-rules ()
+;;    ((parameterize ("step")
+;;                   ((param value p old new) ...)
+;;                   ()
+;;                   body)
+;;     (let ((p param) ...)
+;;       (let ((old (p)) ...
+;;             (new ((p <param-convert>) value)) ...)
+;;        (dynamic-wind
+;;          (lambda () (p <param-set!> new) ...)
+;;          (lambda () . body)
+;;          (lambda () (p <param-set!> old) ...)))))
+;;    ((parameterize ("step")
+;;                   args
+;;                   ((param value) . rest)
+;;                   body)
+;;     (parameterize ("step")
+;;                   ((param value p old new) . args)
+;;                   rest
+;;                   body))
+;;    ((parameterize ((param value) ...) . body)
+;;     (parameterize ("step")
+;;                   ()
+;;                   ((param value) ...)
+;;                   body))))
+
