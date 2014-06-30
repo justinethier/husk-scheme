63257a4eb0298b8abb046440a9e6a80143ab3c47
Justin Ethier <justin.ethier@gmail.com>
2014-06-30 17:55:07 -0400

    Issue #162 - Added tests from pattern match lambda

--- tests/t-macro.scm
+++ tests/t-macro.scm
@@ -531,4 +531,75 @@
                    ; correctly implemented, not ((1) (2) (3) (4))
 ; END SRFI 46
 
+(import (libs pattern-match-lambda))
+;; Test cases from issue #162
+(let ()
+    (define fact
+      (pattern-match-lambda ()
+       ((0) 1)
+       ((n) (* n (fact (- n 1))))))
+    
+    (assert/equal (fact 5) 120)
+    
+     (define example
+       (pattern-match-lambda ()
+         ((x y z) (list 'case1 x y z))
+         ((x (y z)) (list 'case2 x y z))
+         (((x y) z) (list 'case3 x y z))
+         (else 'case3)))
+     
+    ;(write (expand (example 1 2 3)))
+    
+    (assert/equal (example 1 2 3) '(case1 1 2 3))
+    (assert/equal (example 4 '(5 6)) '(case2 4 5 6))
+    (assert/equal (example '(7 8) 9) '(case3 7 8 9))
+    (assert/equal (example 10 11 12 13) 'case3)
+     
+    (define example2
+      (pattern-match-lambda (foo bar baz)
+        ((foo 1) 'foo-case-1)
+        ((foo 2) 'foo-case-2)
+        ((foo (x #(y z))) (list 'foo-case x y z))
+        ((bar x) (list 'bar-case x))
+        ((baz x) (list 'baz-case x))
+        (else 'else-case)))
+    
+    (assert/equal (example2 'foo 1) 'foo-case-1)
+    (assert/equal (example2 'foo '(1 #(2 3))) '(foo-case 1 2 3))
+    (assert/equal (example2 'foo 2) 'foo-case-2)
+    (assert/equal (example2 'baz 4) '(baz-case 4))
+    
+    ; TODO:
+    ;; Underbar is placeholder
+    (define example3
+      (pattern-match-lambda ()
+        ((_) 'arity1)
+        ((_ _) 'arity2)
+        ((_ _ _) 'arity3)))
+    
+    (assert/equal (example3 1 1 1) 'arity3)
+    
+    ;; If underbar was specified as literal, underbar will match literal.
+    (define example4
+      (pattern-match-lambda (_)
+        ((_) 'case1)
+        ((x) x)))
+    
+    (assert/equal (example4 '_) 'case1)
+    (assert/equal (example4 'foo) 'foo)
+    
+    ;; If there is duplicate template variables, report error.
+    ;; (pattern-match-lambda (_)
+    ;;    ((_) 'case1)
+    ;;    ((x x) x))
+    
+    ;; If there is fender, use it.
+    (define example5
+      (pattern-match-lambda ()
+        ((x) (string? x) x)
+        ((x) 'not-string)))
+    
+    (assert/equal (example5 "1") "1")
+    (assert/equal (example5 1) 'not-string))
+
 (unit-test-handler-results)
