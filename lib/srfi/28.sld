2ce74a092b2ea579dca74355b5a4ea3442a6ae1b
Justin Ethier <justin.ethier@gmail.com>
2014-07-08 13:14:22 -0400

    Issue #148 - Initial files

---
+++ lib/srfi/28.sld
@@ -0,0 +1,7 @@
+(define-library (srfi 28)
+    (export
+        format)
+    (import 
+        (scheme base)
+        (scheme write))
+    (include "srfi-28.scm"))
---
+++ lib/srfi/srfi-28.scm
@@ -0,0 +1,38 @@
+;;;
+;;; husk-scheme
+;;; http://justinethier.github.com/husk-scheme
+;;;
+;;; Implementation of SRFI-28: Basic Format Strings
+;;;
+(define format
+  (lambda (format-string . objects)
+    (let ((buffer (open-output-string)))
+      (let loop ((format-list (string->list format-string))
+                 (objects objects))
+        (cond ((null? format-list) (get-output-string buffer))
+              ((char=? (car format-list) #\~)
+               (if (null? (cdr format-list))
+                   (error 'format "Incomplete escape sequence")
+                   (case (cadr format-list)
+                     ((#\a)
+                      (if (null? objects)
+                          (error 'format "No value for escape sequence")
+                          (begin
+                            (display (car objects) buffer)
+                            (loop (cddr format-list) (cdr objects)))))
+                 ((#\s)
+                      (if (null? objects)
+                          (error 'format "No value for escape sequence")
+                          (begin
+                            (write (car objects) buffer)
+                            (loop (cddr format-list) (cdr objects)))))
+                     ((#\%)
+                      (newline buffer)
+                      (loop (cddr format-list) objects))
+                     ((#\~)
+                      (write-char #\~ buffer)
+                      (loop (cddr format-list) objects))
+                     (else
+                      (error 'format "Unrecognized escape sequence")))))
+              (else (write-char (car format-list) buffer)
+                    (loop (cdr format-list) objects)))))))
