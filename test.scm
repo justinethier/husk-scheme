30bfb832a0816a66e890a038cf875c93067cc4aa
Justin Ethier <justin.ethier@gmail.com>
2015-01-26 22:46:42 -0500

    Added a TODO list for this branch

--- test.scm
+++ test.scm
@@ -1,4 +1,11 @@
 ;; Temporary test file, delete before merging back to master!!
+;; TODO list:
+;;
+;; - Compare running this in husk to chicken, there are calls missing (EG: if)
+;; - Fix compiler warnings
+;; - Improve call history formatting, maybe use a common function between Core/Types?
+;; - Compare speed of this branch against master, can anything be sped up?
+;; - What else before merging back?
 (define a '(1 2 3))
 (define (loop i)
   (if (= i 10)
