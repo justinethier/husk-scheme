e8cf09107661696871b017906e16f87c65e4a8b4
Justin Ethier <justin.ethier@gmail.com>
2015-01-28 18:05:15 -0500

    Added notes, big memory issue with call hist

--- test.scm
+++ test.scm
@@ -1,14 +1,15 @@
 ;; Temporary test file, delete before merging back to master!!
 ;; TODO list:
 ;;
-;; - Compare running this in husk to chicken, there are calls missing (EG: if)
+;; - Uh oh, running tail-call-opt example shows unbound memory growth!!
 ;; - Compare speed of this branch against master, can anything be sped up?
-;; - Before merging back: check docs, eliminate compiler warnings
+;;   maybe try running the profiler some more too, esp for computationally-intensive code
 ;; - Compiler does not produce any call history
 ((lambda ()
     (list? (list))
     (define a '(1 2 3))
     (define (loop i)
+      ;(if (= i 10)
       (if (read-char a) ;(= i 10)
         (read-char a)
         (loop (+ i 1))))
