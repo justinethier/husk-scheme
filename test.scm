48aa32e9f7d6c873e201432c1820ac24ca548c28
Justin Ethier <justin.ethier@gmail.com>
2015-01-29 13:11:47 -0500

    Fixed unbounded memory problem

--- test.scm
+++ test.scm
@@ -1,7 +1,6 @@
 ;; Temporary test file, delete before merging back to master!!
 ;; TODO list:
 ;;
-;; - Uh oh, running tail-call-opt example shows unbound memory growth!!
 ;; - Compare speed of this branch against master, can anything be sped up?
 ;;   maybe try running the profiler some more too, esp for computationally-intensive code
 ;; - Compiler does not produce any call history
